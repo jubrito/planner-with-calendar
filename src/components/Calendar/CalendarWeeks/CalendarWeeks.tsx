dff79e361367a7d1f7d61a46f8171f0000d4ff5c
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-02-27 22:27:03 -0300

    feat: allow screen reader to read week name

--- src/components/Calendar/CalendarWeeks/CalendarWeeks.tsx
+++ src/components/Calendar/CalendarWeeks/CalendarWeeks.tsx
@@ -1,13 +1,17 @@
-import { weekDaysWithAbbreviation } from "../../../utils/constants";
+import { weekDays, weekDaysWithAbbreviation } from "../../../utils/constants";
 import styles from "./_calendar-weeks.module.scss";
 
 const CalendarWeeks = () => {
   return (
     <div className={styles.weekDaysContainer}>
-      {weekDaysWithAbbreviation.map((weekDay) => {
+      {weekDaysWithAbbreviation.map((weekDay, weekDayIndex) => {
         return (
-          <div key={weekDay} className={styles.weekDays}>
-            {weekDay}
+          <div
+            key={weekDay}
+            className={styles.weekDays}
+            aria-label={weekDays[weekDayIndex]}
+          >
+            <span aria-hidden="true">{weekDay}</span>
           </div>
         );
       })}
