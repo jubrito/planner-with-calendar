dc813c9db48375b10177925ce49fd239028113eb
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-13 21:22:09 -0300

    test: display leap year may calendar cells correctly

--- src/components/Calendar/CalendarCells/CalendarCells.spec.tsx
+++ src/components/Calendar/CalendarCells/CalendarCells.spec.tsx
@@ -772,5 +772,49 @@ describe("CalendarCells", () => {
         });
       });
     });
+    describe("May", () => {
+      const currentMonthNumberOfDays = 31;
+      beforeEach(() => {
+        const mockUseDate = getUseDateMock(
+          leapYear,
+          Months.MAY,
+          1,
+          currentMonthNumberOfDays
+        );
+        render(<CalendarCells dateConfig={mockUseDate} />);
+      });
+
+      it("should not render days from April (previous month) to fill calendar", () => {
+        const aprilDays: number[] = [];
+
+        aprilDays.forEach((aprilDay) => {
+          const dayCell = screen.getByTitle(`${leapYear}-${4}-${aprilDay}`);
+          expect(dayCell).toBeInTheDocument();
+          expect(dayCell.textContent).toBe(aprilDay.toString());
+        });
+      });
+
+      it("should render days from May (current month) to fill calendar", () => {
+        const mayDays = Array.from(
+          Array(currentMonthNumberOfDays).keys(),
+          (day) => day + 1
+        );
+        mayDays.forEach((mayDay) => {
+          const dayCell = screen.getByTitle(`${leapYear}-${5}-${mayDay}`);
+          expect(dayCell).toBeInTheDocument();
+          expect(dayCell.textContent).toBe(mayDay.toString());
+        });
+      });
+
+      it("should render days from June (next month) to fill calendar", () => {
+        const juneDays = [1, 2, 3, 4];
+
+        juneDays.forEach((juneDay) => {
+          const dayCell = screen.getByTitle(`${leapYear}-${6}-${juneDay}`);
+          expect(dayCell).toBeInTheDocument();
+          expect(dayCell.textContent).toBe(juneDay.toString());
+        });
+      });
+    });
   });
 });
