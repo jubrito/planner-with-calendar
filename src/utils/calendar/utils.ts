fee102afcd3ed168a0742a3150c72828803b69c5
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-14 00:19:54 -0300

    feat: allow getting monh name by index

--- src/utils/calendar/utils.ts
+++ src/utils/calendar/utils.ts
@@ -1,4 +1,8 @@
-import { WeekDaysShortNames } from "../../types/calendar/enums";
+import {
+  Months,
+  MonthsNames,
+  WeekDaysShortNames,
+} from "../../types/calendar/enums";
 import { getWeekDaysNames } from "./weeks";
 
 export const numOfDaysFromOtherMonthOnCurrentCalendar = (
@@ -6,3 +10,18 @@ export const numOfDaysFromOtherMonthOnCurrentCalendar = (
   locale: string
 ) => getWeekDaysNames(locale).findIndex((name) => weekDayName === name.short);
 export const firstDayOfTheMonth = 1;
+
+export const monthNameByIndex: Record<Months, MonthsNames> = {
+  [Months.JANUARY]: MonthsNames.JANUARY,
+  [Months.FEBRUARY]: MonthsNames.FEBRUARY,
+  [Months.MARCH]: MonthsNames.MARCH,
+  [Months.APRIL]: MonthsNames.APRIL,
+  [Months.MAY]: MonthsNames.MAY,
+  [Months.JUNE]: MonthsNames.JUNE,
+  [Months.JULY]: MonthsNames.JULY,
+  [Months.AUGUST]: MonthsNames.AUGUST,
+  [Months.SEPTEMBER]: MonthsNames.SEPTEMBER,
+  [Months.OCTOBER]: MonthsNames.OCTOBER,
+  [Months.NOVEMBER]: MonthsNames.NOVEMBER,
+  [Months.DECEMBER]: MonthsNames.DECEMBER,
+};
