92addc9ca393ae861a26cac0399b6754653fd504
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-18 22:36:22 -0300

    feat: wrap app in redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./styles/_main.scss";
 import Home from "./pages/Home/Home";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
 };
 
 export default App;
