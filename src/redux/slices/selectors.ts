21e6eaec386a9b16bf8c6fba5c541a4c2184cca0
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-18 23:16:56 -0300

    feat: create get month number of days selector

--- src/redux/slices/selectors.ts
+++ src/redux/slices/selectors.ts
@@ -3,21 +3,26 @@ import { createSelector } from "@reduxjs/toolkit";
 
 const updateDateState = (store: RootState) => store.updateDate;
 
-export const getDate = () =>
+export const getCurrentDate = () =>
   createSelector(updateDateState, (state) => state.currentState.date);
 
-export const getDay = () =>
+export const getCurrentDay = () =>
   createSelector(updateDateState, (state) => state.currentState.date.getDate());
 
-export const getMonth = () =>
+export const getCurrentMonth = () =>
   createSelector(updateDateState, (state) =>
     state.currentState.date.getMonth()
   );
 
-export const getYear = () =>
+export const getCurrentYear = () =>
   createSelector(updateDateState, (state) =>
     state.currentState.date.getFullYear()
   );
 
-export const getTime = () =>
-  createSelector(updateDateState, (state) => state.currentState.date.getTime());
+export const getCurrentMonthNumberOfDays = () =>
+  createSelector(updateDateState, (state) => {
+    const year = state.currentState.date.getFullYear();
+    const month = state.currentState.date.getMonth() + 1;
+    const getLastDayOfMonth = 0;
+    return new Date(year, month, getLastDayOfMonth).getDate();
+  });
