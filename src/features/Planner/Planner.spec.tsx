34bb6a9ce65132157c8184e69bf068d2b9de59b5
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-23 20:45:02 -0300

    test: should render header initial date text

---
+++ src/features/Planner/Planner.spec.tsx
@@ -0,0 +1,28 @@
+import "@testing-library/jest-dom";
+import { screen } from "@testing-library/dom";
+import { initialValue } from "../../redux/slices/dateSlice";
+import { Months } from "../../types/calendar/enums";
+import { renderWithProviders } from "../../utils/tests/renderWithProviders";
+import Planner from "./Planner";
+
+describe("Planner", () => {
+  const currentYear = 2025;
+  describe("Header", () => {
+    it("should render header initial date text", () => {
+      renderWithProviders(<Planner />, {
+        preloadedState: {
+          dateSlice: {
+            initialState: {
+              ...initialValue.initialState,
+              date: new Date(currentYear, Months.MARCH, 1).toDateString(),
+            },
+            currentState: {
+              ...initialValue.currentState,
+            },
+          },
+        },
+      });
+      expect(screen.getByText("Mar 1, Sat")).toBeInTheDocument();
+    });
+  });
+});
