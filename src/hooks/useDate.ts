3e65d29773b90c9633cfff997f09906f72d2ec54
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-23 20:05:21 -0300

    feat: create use date hook

---
+++ src/hooks/useDate.ts
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { DateConfig } from "../types/calendar/types";
+import { LocaleLanguage } from "../types/locale/types";
+
+const getInitialDate = (
+  initialYear?: number,
+  initialMonth?: number,
+  initialDay?: number
+) => {
+  const year = initialYear ?? new Date().getFullYear();
+  const month = initialMonth ?? new Date().getMonth();
+  const day = initialDay ?? new Date().getDate();
+
+  return new Date(year, month, day);
+};
+
+export const useDate = (
+  locale: LocaleLanguage,
+  initialYear?: DateConfig["year"],
+  initialMonth?: DateConfig["month"],
+  initialDay?: DateConfig["day"]
+): DateConfig => {
+  const [date, setDate] = useState(
+    getInitialDate(initialYear, initialMonth, initialDay)
+  );
+
+  const updateDate = (year: number, month: number, day: number) => {
+    setDate(new Date(year, month, day));
+  };
+
+  return {
+    date,
+    updateDate,
+    day: date.getDate(),
+    month: date.getMonth(),
+    year: date.getFullYear(),
+    time: date.getTime(),
+    dayOfWeek: new Intl.DateTimeFormat(locale, {
+      weekday: "long",
+    }).format(date),
+    monthNumberOfDays: new Date(
+      date.getFullYear(),
+      date.getMonth() + 1,
+      0
+    ).getDate(),
+  };
+};
