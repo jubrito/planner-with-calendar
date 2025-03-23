4ad8c8f8d8280e1d50eda0bcc00798bbf88f55f7
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-23 19:09:26 -0300

    test: should render footer attribution text

--- src/features/Footer/Footer.spec.tsx
+++ src/features/Footer/Footer.spec.tsx
@@ -4,18 +4,15 @@ import Footer from "./Footer";
 import "@testing-library/jest-dom";
 
 describe("Footer", () => {
-  it("should render footer attribution with link", () => {
+  beforeEach(() => {
     render(<Footer />);
+  });
+
+  it("should render footer attribution text", () => {
     const designedAndDevelopedByText = "Designed and developed by";
-    const attributionLinkLabel =
-      "Go to Juliana Witzke de Brito's LinkedIn page";
     const designedAndDevelopedByElement = screen.getByText(
       designedAndDevelopedByText
     );
-    const attributionLinkElement = screen.getByRole("link", {
-      name: attributionLinkLabel,
-    });
     expect(designedAndDevelopedByElement).toBeInTheDocument();
-    expect(attributionLinkElement).toBeInTheDocument();
   });
 });
