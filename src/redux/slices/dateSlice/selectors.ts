eb63a0d1d40d8feb7e64641fd09095d547dacfce
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-23 20:21:16 -0300

    feat: create getCurrentMonthNumberOfDays selector

--- src/redux/slices/dateSlice/selectors.ts
+++ src/redux/slices/dateSlice/selectors.ts
@@ -49,6 +49,15 @@ export const getSelectedTime = () =>
     new Date(state.currentState.date).getTime()
   );
 
+export const getCurrentMonthNumberOfDays = () =>
+  createSelector(updateDateState, (state) => {
+    const currentDate = new Date(state.initialState.date);
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    const getLastDayOfMonth = 0;
+    return new Date(year, month, getLastDayOfMonth).getDate();
+  });
+
 export const getSelectedMonthNumberOfDays = () =>
   createSelector(updateDateState, (state) => {
     const currentDate = new Date(state.currentState.date);
