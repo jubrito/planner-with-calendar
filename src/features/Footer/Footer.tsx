80cc93ab586c837c8dd40734f25337b1a4a34bfe
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-23 18:57:31 -0300

    feat: open linkedin on new tab

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -4,7 +4,11 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       Designed and developed by
-      <a href="https://www.linkedin.com/in/jubrito" rel="noopener noreferrer">
+      <a
+        href="https://www.linkedin.com/in/jubrito"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
         Juliana Witzke de Brito
       </a>
     </footer>
