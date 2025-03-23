7b0c65742af4b7a91d550cfa3ab3879348780576
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-23 18:58:43 -0300

    feat: wrap footer text in a paragraph element

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -3,15 +3,17 @@ import styles from "./_footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Designed and developed by
-      <a
-        href="https://www.linkedin.com/in/jubrito"
-        rel="noopener noreferrer"
-        target="_blank"
-        aria-label="Go to Juliana Witzke de Brito's linkedIn"
-      >
-        Juliana Witzke de Brito
-      </a>
+      <p>
+        Designed and developed by
+        <a
+          href="https://www.linkedin.com/in/jubrito"
+          rel="noopener noreferrer"
+          target="_blank"
+          aria-label="Go to Juliana Witzke de Brito's linkedIn"
+        >
+          Juliana Witzke de Brito
+        </a>
+      </p>
     </footer>
   );
 };
