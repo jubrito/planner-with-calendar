3fc44848f0c03d25e7fade4bf5e0e4f64e827df8
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-23 18:50:32 -0300

    feat: add footer description

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -1,7 +1,14 @@
 import styles from "./_footer.module.scss";
 
 const Footer = () => {
-  return <footer className={styles.footer}>Placeholder footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      Designed and developed by
+      <a href="https://www.linkedin.com/in/jubrito" rel="noopener noreferrer">
+        Juliana Witzke de Brito
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
