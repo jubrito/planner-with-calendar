60a09d2d5755f4320216e5271b3189ab8dd3e79e
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-23 19:50:18 -0300

    feat: add initial planner header placeholder

--- src/features/Planner/Planner.tsx
+++ src/features/Planner/Planner.tsx
@@ -1,7 +1,15 @@
 import styles from "./_planner.module.scss";
 
 const Planner = () => {
-  return <section className={styles.planner}>Placeholder planner</section>;
+  return (
+    <section className={styles.planner}>
+      <div className={styles.plannerHeader}>
+        <h2 className={styles.plannerHeaderLabel} id="calendar-month-name">
+          March, 2025
+        </h2>
+      </div>
+    </section>
+  );
 };
 
 export default Planner;
