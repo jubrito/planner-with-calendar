d9b6b867eb5cf0803bbc3cdc991d816fc0d28508
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-23 20:11:50 -0300

    feat: update planner initial structure

--- src/features/Planner/Planner.tsx
+++ src/features/Planner/Planner.tsx
@@ -1,11 +1,25 @@
+import { useSelector } from "react-redux";
 import styles from "./_planner.module.scss";
+import {
+  getCurrentDay,
+  getCurrentMonth,
+  getCurrentYear,
+} from "../../redux/slices/dateSlice/selectors";
+import { useDate } from "../../hooks/useDate";
+import { getLocaleLanguage } from "../../redux/slices/localeSlice/selectors";
 
 const Planner = () => {
+  const localeLang = useSelector(getLocaleLanguage());
+  const year = useSelector(getCurrentYear());
+  const month = useSelector(getCurrentMonth());
+  const day = useSelector(getCurrentDay());
+  const { dayOfWeek } = useDate(localeLang, year, month, day);
+
   return (
     <section className={styles.planner}>
       <div className={styles.plannerHeader}>
         <h2 className={styles.plannerHeaderLabel} id="calendar-month-name">
-          March, 2025
+          {dayOfWeek.long}
         </h2>
       </div>
     </section>
