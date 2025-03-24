d64dc44404646aa3177ff70f4bcf6035f7509fa5
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-24 00:49:20 -0300

    chore: add 2-digit intl value

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,4 @@ export const IntlDateTimeFormatLong = "long";
 export const IntlDateTimeFormatMedium = "medium";
 export const IntlDateTimeFormatShort = "short";
 export const IntlDateTimeFormatNumeric = "numeric";
+export const IntlDateTimeFormat2Digit = "2-digit";
