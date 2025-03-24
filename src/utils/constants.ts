e8f528cbc898e0febe166dd652ed3520e526faaf
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-24 01:27:19 -0300

    feat: create narrow intl type

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,4 @@ export const IntlDateTimeFormatMedium = "medium";
 export const IntlDateTimeFormatShort = "short";
 export const IntlDateTimeFormatNumeric = "numeric";
 export const IntlDateTimeFormat2Digit = "2-digit";
+export const IntlDateTimeFormatNarrow = "narrow";
