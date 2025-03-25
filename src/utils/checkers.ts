68df5b40bc85b6840a71613d5dcd38e3adce4621
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-24 23:32:03 -0300

    refactor: use get formated date on is today checker

--- src/utils/checkers.ts
+++ src/utils/checkers.ts
@@ -1,9 +1,10 @@
 import { DateConfig } from "../types/calendar/types";
 import { LocaleLanguage } from "../types/locale/types";
+import { getFormatedDate } from "./calendar/current";
 import { getDay, getMonthIndex, getYear } from "./calendar/utils";
 
 export const isToday = (locale: LocaleLanguage, date: DateConfig["date"]) => {
-  const currentDate = new Date();
+  const currentDate = getFormatedDate(locale, new Date());
   return (
     getYear(locale, date) === getYear(locale, currentDate) &&
     getMonthIndex(locale, date) === getMonthIndex(locale, currentDate) &&
