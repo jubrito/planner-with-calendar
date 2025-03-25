d0bec1e693c90f1ffd802d2a6f1e0e50e12a83a7
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-25 00:56:29 -0300

    fix: pass date to hook

--- src/features/Planner/Planner.tsx
+++ src/features/Planner/Planner.tsx
@@ -1,25 +1,14 @@
 import { useSelector } from "react-redux";
 import styles from "./_planner.module.scss";
-import {
-  getInitialDay,
-  getInitialMonth,
-  getInitialYear,
-} from "../../redux/slices/dateSlice/selectors";
+import { getInitialDate } from "../../redux/slices/dateSlice/selectors";
 import { useDate } from "../../hooks/useDate";
 import { getLocaleLanguage } from "../../redux/slices/localeSlice/selectors";
 import { getMonthName } from "../../utils/calendar/utils";
 
 const Planner = () => {
   const locale = useSelector(getLocaleLanguage());
-  const initialYear = useSelector(getInitialYear(locale));
-  const initialMonth = useSelector(getInitialMonth(locale));
-  const initialDay = useSelector(getInitialDay(locale));
-  const { date, day, dayOfWeek } = useDate(
-    locale,
-    initialYear,
-    initialMonth,
-    initialDay
-  );
+  const initialDate = useSelector(getInitialDate(locale));
+  const { date, day, dayOfWeek } = useDate(locale, initialDate);
   const monthName = getMonthName(locale, date, "short");
 
   return (
