0d265866444c1cd72ae4adcd9e6853efcabde804
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-26 20:09:18 -0300

    test: WIP should display hours of the day using 24-hour notation when locale is portuguese

--- src/features/Planner/Planner.spec.tsx
+++ src/features/Planner/Planner.spec.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/dom';
-import { initialValue } from '../../redux/slices/dateSlice';
+import { initialValue as initialDateValue } from '../../redux/slices/dateSlice';
+import { initialValue as initialLocaleValue } from '../../redux/slices/localeSlice';
 import { Months } from '../../types/calendar/enums';
 import { renderWithProviders } from '../../utils/tests/renderWithProviders';
 import Planner from './Planner';
@@ -13,11 +14,11 @@ describe('Planner', () => {
         preloadedState: {
           dateSlice: {
             initialState: {
-              ...initialValue.initialState,
+              ...initialDateValue.initialState,
               date: new Date(currentYear, Months.MARCH, 1).toDateString(),
             },
             currentState: {
-              ...initialValue.currentState,
+              ...initialDateValue.currentState,
             },
           },
         },
@@ -29,11 +30,11 @@ describe('Planner', () => {
         preloadedState: {
           dateSlice: {
             initialState: {
-              ...initialValue.initialState,
+              ...initialDateValue.initialState,
               date: new Date(currentYear, Months.MARCH, 1).toDateString(),
             },
             currentState: {
-              ...initialValue.currentState,
+              ...initialDateValue.currentState,
             },
           },
         },
@@ -67,6 +68,61 @@ describe('Planner', () => {
         expect(screen.getByText(hour)).toBeInTheDocument();
       });
       expect(screen.getAllByText('12 AM').length).toBe(2);
+    });
+    it.only('should display hours of the day using 24-hour notation when locale is portuguese', () => {
+      renderWithProviders(<Planner />, {
+        preloadedState: {
+          dateSlice: {
+            initialState: {
+              ...initialDateValue.initialState,
+              date: new Date(currentYear, Months.MARCH, 1).toDateString(),
+            },
+            currentState: {
+              ...initialDateValue.currentState,
+            },
+          },
+          localeSlice: {
+            currentState: {
+              ...initialLocaleValue.currentState,
+              locale: {
+                lang: 'pt-BR',
+              },
+            },
+            initialState: {
+              ...initialLocaleValue.initialState,
+            },
+          },
+        },
+      });
+      const hours = [
+        '01:00',
+        '02:00',
+        '03:00',
+        '04:00',
+        '05:00',
+        '06:00',
+        '07:00',
+        '08:00',
+        '09:00',
+        '10:00',
+        '11:00',
+        '12:00',
+        '13:00',
+        '14:00',
+        '15:00',
+        '16:00',
+        '17:00',
+        '18:00',
+        '19:00',
+        '20:00',
+        '21:00',
+        '22:00',
+        '23:00',
+        '24:00',
+      ];
+      hours.forEach((hour) => {
+        expect(screen.getByText(hour)).toBeInTheDocument();
+      });
     });
   });
 });
