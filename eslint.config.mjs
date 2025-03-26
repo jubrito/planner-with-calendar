1ff385a9513c81c0f77215c45b6ac2a3b0dfb06a
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-25 21:38:23 -0300

    chore: configure eslint initial structure

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.strict,
+  tseslint.configs.recommended,
+  tseslint.configs.stylistic,
+);
