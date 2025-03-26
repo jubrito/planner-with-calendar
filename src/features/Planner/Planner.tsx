511cb7667b99b167bb1004b83c929e1e39895482
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-26 00:40:06 -0300

    style: define fixed height for planner and calendar areas

--- src/features/Planner/Planner.tsx
+++ src/features/Planner/Planner.tsx
@@ -1,15 +1,17 @@
-import { useSelector } from "react-redux";
-import styles from "./_planner.module.scss";
-import { getInitialDate } from "../../redux/slices/dateSlice/selectors";
-import { useDate } from "../../hooks/useDate";
-import { getLocaleLanguage } from "../../redux/slices/localeSlice/selectors";
-import { getMonthName } from "../../utils/calendar/utils";
+import { useSelector } from 'react-redux';
+import styles from './_planner.module.scss';
+import { getInitialDate } from '../../redux/slices/dateSlice/selectors';
+import { useDate } from '../../hooks/useDate';
+import { getLocaleLanguage } from '../../redux/slices/localeSlice/selectors';
+import { getHoursOfTheDay, getMonthName } from '../../utils/calendar/utils';
 
 const Planner = () => {
   const locale = useSelector(getLocaleLanguage());
   const initialDate = useSelector(getInitialDate(locale));
-  const { date, day, dayOfWeek } = useDate(locale, initialDate);
-  const monthName = getMonthName(locale, date, "short");
+  const { date, year, month, day, dayOfWeek } = useDate(locale, initialDate);
+  const monthName = getMonthName(locale, date, 'short');
+
+  const hoursOfTheDay = getHoursOfTheDay(locale, year, month, day);
 
   return (
     <section className={styles.planner}>
@@ -17,6 +19,13 @@ const Planner = () => {
         <h2 className={styles.plannerHeaderLabel} id="calendar-month-name">
           {`${monthName} ${day}, ${dayOfWeek}`}
         </h2>
+      </div>
+      <div className={styles.hoursWrapper}>
+        <div className={styles.hoursOfTheDay}>
+          {hoursOfTheDay.map((hourOfTheDay) => {
+            return <span>{hourOfTheDay}</span>;
+          })}
+        </div>
       </div>
     </section>
   );
