28eb1324eb7a6aeab3d58b2614cf2a99464cfe6a
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-27 00:53:18 -0300

    feat: add label and title to planner hours

--- src/features/Planner/Planner.tsx
+++ src/features/Planner/Planner.tsx
@@ -3,7 +3,11 @@ import styles from './_planner.module.scss';
 import { getInitialDate } from '../../redux/slices/dateSlice/selectors';
 import { useDate } from '../../hooks/useDate';
 import { getLocaleLanguage } from '../../redux/slices/localeSlice/selectors';
-import { getHoursOfTheDay, getMonthName } from '../../utils/calendar/utils';
+import {
+  getFullDateTitle,
+  getHoursOfTheDay,
+  getMonthName,
+} from '../../utils/calendar/utils';
 import { IntlDateTimeFormatShort } from '../../utils/constants';
 
 const Planner = () => {
@@ -11,14 +15,14 @@ const Planner = () => {
   const initialDate = useSelector(getInitialDate());
   const { date, year, month, day, dayOfWeek } = useDate(locale, initialDate);
   const monthName = getMonthName(locale, date, IntlDateTimeFormatShort);
-
   const hoursOfTheDay = getHoursOfTheDay(locale, year, month, day);
+  const plannerDateLabel = `${monthName} ${day}, ${dayOfWeek}`;
 
   return (
     <section className={styles.planner}>
       <div className={styles.plannerHeader}>
         <h2 className={styles.plannerHeaderLabel} id="calendar-month-name">
-          {`${monthName} ${day}, ${dayOfWeek}`}
+          {plannerDateLabel}
         </h2>
       </div>
       <div className={styles.hoursWrapper}>
@@ -29,6 +33,8 @@ const Planner = () => {
                 <div
                   className={styles.hoursOfTheDayRow}
                   key={hourOfTheDay + index}
+                  aria-label={`${getFullDateTitle(year, month, day, locale)} ${hourOfTheDay}`}
+                  title={`${getFullDateTitle(year, month, day, locale)} ${hourOfTheDay}`}
                 >
                   <span className={styles.hoursOfTheDay}>{hourOfTheDay}</span>
                   <span className={styles.hoursOfTheDayLine}></span>
