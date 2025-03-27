67218148ebc98d73241bab8644c2969bee1d1b6e
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-27 00:31:53 -0300

    chore: change interface doc note

--- src/utils/tests/renderWithProviders.tsx
+++ src/utils/tests/renderWithProviders.tsx
@@ -1,20 +1,22 @@
-import { PropsWithChildren } from "react";
-import { render } from "@testing-library/react";
-import type { RenderOptions } from "@testing-library/react";
-import type { AppStore, RootState } from "../../redux/store";
-import setupStore from "../../redux/store";
-import { Provider } from "react-redux";
+import { PropsWithChildren } from 'react';
+import { render } from '@testing-library/react';
+import type { RenderOptions } from '@testing-library/react';
+import type { AppStore, RootState } from '../../redux/store';
+import setupStore from '../../redux/store';
+import { Provider } from 'react-redux';
 
-// This type interface extends the default options for render from RTL, as well
-// as allows the user to specify other things such as initialState, store.
-interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
+/**
+ * This type interface extends the default options for render from RTL, as well
+ *  as allows the user to specify other things such as initialState, store.
+ */
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
   preloadedState?: Partial<RootState>;
   store?: AppStore;
 }
 
 export function renderWithProviders(
   ui: React.ReactElement,
-  extendedRenderOptions: ExtendedRenderOptions = {}
+  extendedRenderOptions: ExtendedRenderOptions = {},
 ) {
   const {
     preloadedState = {},
