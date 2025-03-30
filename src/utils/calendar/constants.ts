c2859880ea600a00158af8c1606ebef8d880d22f
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-03-30 19:20:11 -0300

    create initial and end relative position const values

--- src/utils/calendar/constants.ts
+++ src/utils/calendar/constants.ts
@@ -3,3 +3,5 @@ export const todayLabel = 'Today';
 export const mouseUpEventHandlerType = 'mouseUp';
 export const mouseDownEventHandlerType = 'mouseDown';
 export const mouseLeaveEventHandlerType = 'mouseLeave';
+export const initialRelativePosition = 'initial';
+export const endRelativePosition = 'end';
