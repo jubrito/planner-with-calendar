94100a35d1e5f32bba9c233231705fde968a61bf
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-01 20:54:54 -0300

    move checker to checkers file

--- src/utils/checkers.ts
+++ src/utils/checkers.ts
@@ -1,4 +1,4 @@
-import { DateConfig } from '../types/calendar/types';
+import { DateConfig, ObjectType } from '../types/calendar/types';
 import { LocaleLanguage } from '../types/locale/types';
 import { getDay, getMonthIndex, getYear } from './calendar/utils';
 
@@ -9,4 +9,17 @@ export const isToday = (locale: LocaleLanguage, date: DateConfig['date']) => {
     getMonthIndex(locale, date) === getMonthIndex(locale, currentDate) &&
     getDay(date) === getDay(currentDate)
   );
+};
+export const isObject = (obj: unknown): obj is ObjectType => {
+  if (typeof obj != 'object' || obj === null || obj == null) return false;
+
+  // Ensure it is plain object, not subclass such ass Array and Date
+  const prototype = Object.getPrototypeOf(obj);
+  if (prototype !== Object.prototype) return false;
+
+  for (const key in obj) {
+    if (typeof key !== 'string') return false;
+  }
+
+  return true;
 };
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,18 +1,5 @@
 import { ObjectType } from '../types/calendar/types';
-
-export const isObject = (obj: unknown): obj is ObjectType => {
-  if (typeof obj != 'object' || obj === null || obj == null) return false;
-
-  // Ensure it is plain object, not subclass such ass Array and Date
-  const prototype = Object.getPrototypeOf(obj);
-  if (prototype !== Object.prototype) return false;
-
-  for (const key in obj) {
-    if (typeof key !== 'string') return false;
-  }
-
-  return true;
-};
+import { isObject } from './checkers';
 
 export const deepCopy = <T extends ObjectType>(element: T): T => {
   const copy = {} as T;
