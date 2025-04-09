9a34cf952aa9f9adbad9423b14e374ea6da5bfae
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-08 23:08:10 -0300

    update consts to calculate fifteen min blocks

--- src/utils/calendar/constants.ts
+++ src/utils/calendar/constants.ts
@@ -5,5 +5,6 @@ export const mouseDownEventHandlerType = 'mouseDown';
 export const mouseLeaveEventHandlerType = 'mouseLeave';
 export const initialRelativePosition = 'initial';
 export const endRelativePosition = 'end';
-export const fifteenMinutesItemsInAnHour = 4; // To later divide 1h block by 4 getting 4 blocks of 15 min each
 export const numberOfHoursInADay = 24;
+export const fifteenMinBlocksInAHour = 4; // 1h block divided by 4 results in 4 blocks of 15 min each
+export const sizeOfEach15MinBlock = 12.5;
