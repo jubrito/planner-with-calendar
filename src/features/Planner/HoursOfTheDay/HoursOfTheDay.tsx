0bd8699da2b35a6c3e79864611bfafded0d2c078
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-08 23:08:45 -0300

    remove unnecessary code as span has fixed height of 50px

--- src/features/Planner/HoursOfTheDay/HoursOfTheDay.tsx
+++ src/features/Planner/HoursOfTheDay/HoursOfTheDay.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import styles from './_hours-of-the-day.module.scss';
-import { numberOfHoursInADay } from '../../../utils/calendar/constants';
 
 type HoursOfTheDayProps = {
   hoursOfTheDay: string[];
@@ -8,32 +7,15 @@ type HoursOfTheDayProps = {
 
 export const HoursOfTheDay = ({ hoursOfTheDay }: HoursOfTheDayProps) => {
   const hourOfTheDaySpanRef = useRef<HTMLDivElement>(null);
-  const [spanHeight, setSpanHeight] = useState(50);
-
-  useEffect(() => {
-    const plannerHoursDivHeight =
-      hourOfTheDaySpanRef.current?.parentElement?.scrollHeight;
-    if (plannerHoursDivHeight) {
-      setSpanHeight(plannerHoursDivHeight / numberOfHoursInADay);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (hourOfTheDaySpanRef.current?.parentElement) {
-      hourOfTheDaySpanRef.current.style.height = `${hourOfTheDaySpanRef.current.parentElement.scrollHeight}px`;
-    }
-  }, []);
 
   return (
     <div className={styles.hourOfTheDay} ref={hourOfTheDaySpanRef}>
       {hoursOfTheDay.map((hourOfTheDay) => {
         return (
-          <span
-            style={{ height: spanHeight }}
-            key={hourOfTheDay + crypto.randomUUID()}
-          >
-            {hourOfTheDay}
-          </span>
+          <div className={styles.container}>
+            <span>{hourOfTheDay}</span>
+            <hr className={styles.line} />
+          </div>
         );
       })}
     </div>
