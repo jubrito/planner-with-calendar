67c675a3352e78b8084e2d196292e04c54a7a119
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-10 00:23:26 -0300

    remove unused const

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,5 @@
 export const IntlDateTimeFormatFull = 'full';
 export const IntlDateTimeFormatLong = 'long';
-export const IntlDateTimeFormatMedium = 'medium';
 export const IntlDateTimeFormatShort = 'short';
 export const IntlDateTimeFormatNumeric = 'numeric';
 export const IntlDateTimeFormat2Digit = '2-digit';
