8b5d479b44d870045f76258ac9a62c9c3b664e3d
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-10 00:16:53 -0300

    remove unused code of relative positions consts

--- src/utils/calendar/constants.ts
+++ src/utils/calendar/constants.ts
@@ -1,7 +1,5 @@
 export const firstDayOfTheMonth = 1;
 export const todayLabel = 'Today';
-export const initialRelativePosition = 'initial';
-export const endRelativePosition = 'end';
 export const numberOfHoursInADay = 24;
 export const fifteenMinBlocksInAHour = 4; // 1h block divided by 4 results in 4 blocks of 15 min each
 export const sizeOfEach15MinBlock = 12.5;
