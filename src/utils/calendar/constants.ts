9cdc3e7bb9eb6342bea61291053e630dd4d078b2
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-10 00:16:31 -0300

    remove unused code of mouseEventHandlerTypes

--- src/utils/calendar/constants.ts
+++ src/utils/calendar/constants.ts
@@ -1,8 +1,5 @@
 export const firstDayOfTheMonth = 1;
 export const todayLabel = 'Today';
-export const mouseUpEventHandlerType = 'mouseUp';
-export const mouseDownEventHandlerType = 'mouseDown';
-export const mouseLeaveEventHandlerType = 'mouseLeave';
 export const initialRelativePosition = 'initial';
 export const endRelativePosition = 'end';
 export const numberOfHoursInADay = 24;
