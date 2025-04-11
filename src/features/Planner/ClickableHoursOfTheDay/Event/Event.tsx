b6141430df7c255eaed9ca32e10b08b4fd8abfd3
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-11 00:36:20 -0300

    display event info

--- src/features/Planner/ClickableHoursOfTheDay/Event/Event.tsx
+++ src/features/Planner/ClickableHoursOfTheDay/Event/Event.tsx
@@ -1,6 +1,10 @@
+import { useSelector } from 'react-redux';
 import { sizeOfEach15MinBlock } from '../../../../utils/calendar/constants';
+import { getFormatedDateString } from '../../../../utils/calendar/utils';
 import styles from '../clickable-hours-of-the-day.module.scss';
 import { EventBlock } from '../ClickableHoursOfTheDay';
+import { getLocaleLanguage } from '../../../../redux/slices/localeSlice/selectors';
+import { IntlDateTimeFormat2Digit } from '../../../../utils/constants';
 
 type EventProps = {
   event: EventBlock;
@@ -9,7 +13,27 @@ type EventProps = {
 
 export const Event = ({ event, onClick }: EventProps) => {
   const eventHeight = event.end.fixedPositionY - event.start.fixedPositionY;
+  const isAtLeast45MinEvent = eventHeight >= sizeOfEach15MinBlock * 3;
   const hasMinimumHeight = eventHeight >= sizeOfEach15MinBlock;
+  const localeString = useSelector(getLocaleLanguage());
+  const startHour = getFormatedDateString(localeString, event.start.date, {
+    hour: IntlDateTimeFormat2Digit,
+  }).slice(0, 2);
+  const endHour = getFormatedDateString(localeString, event.end.date, {
+    hour: IntlDateTimeFormat2Digit,
+  });
+  const startMinutes = getFormatedDateString(localeString, event.start.date, {
+    minute: IntlDateTimeFormat2Digit,
+  });
+  const endMinutes = getFormatedDateString(localeString, event.end.date, {
+    minute: IntlDateTimeFormat2Digit,
+  });
+  const eventTime = `${startHour}:${startMinutes} – ${endHour}:${endMinutes}`;
+  console.log('startHour', startHour);
+  console.log('startMinutes', startMinutes);
+  console.log('endHour', endHour);
+  console.log('endMinutes', endMinutes);
+
   return (
     <div
       key={event.eventId}
@@ -21,13 +45,31 @@ export const Event = ({ event, onClick }: EventProps) => {
       onClick={onClick}
       onMouseDown={(e) => e.stopPropagation()}
     >
-      <span
-        style={{
-          fontSize: eventHeight === sizeOfEach15MinBlock ? '0.5em' : '1em',
-        }}
-      >
-        {hasMinimumHeight && event.title}
-      </span>
+      {hasMinimumHeight && (
+        <div
+          className={styles.plannerEventDetails}
+          style={{
+            flexDirection: isAtLeast45MinEvent ? 'column' : 'row',
+          }}
+        >
+          <span
+            style={{
+              fontSize: isAtLeast45MinEvent ? '1em' : '0.5em',
+              marginRight: isAtLeast45MinEvent ? 0 : '5px',
+            }}
+          >
+            {event.title}
+          </span>
+          <span
+            style={{
+              fontSize: isAtLeast45MinEvent ? '0.7em' : '0.5em',
+              marginTop: isAtLeast45MinEvent ? '5px' : 0,
+            }}
+          >
+            {eventTime}
+          </span>
+        </div>
+      )}
     </div>
   );
 };
