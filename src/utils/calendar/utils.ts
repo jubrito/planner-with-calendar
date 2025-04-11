ebd39d8072bee6a8ec20880ebbac8e0bf72b4b9b
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-10 22:29:37 -0300

    add space

--- src/utils/calendar/utils.ts
+++ src/utils/calendar/utils.ts
@@ -175,6 +175,7 @@ export const getFormatedDate = (
   date: DateConfig['date'],
   options: Intl.DateTimeFormatOptions = {},
 ) => new Date(getFormatedDateString(locale, date, options));
+
 export const getCurrentMonthDays = (
   year: DateConfig['year'],
   month: DateConfig['month'],
