e3178cd70d76bab190b975b2c91ad276f1934a64
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-10 22:28:05 -0300

    add fifteen min const

--- src/utils/calendar/constants.ts
+++ src/utils/calendar/constants.ts
@@ -4,3 +4,4 @@ export const numberOfHoursInADay = 24;
 export const fifteenMinBlocksInAHour = 4; // 1h block divided by 4 results in 4 blocks of 15 min each
 export const sizeOfEach15MinBlock = 12.5;
 export const numberOfDaysOfTheWeek = 7;
+export const fifteenMinutes = 15;
