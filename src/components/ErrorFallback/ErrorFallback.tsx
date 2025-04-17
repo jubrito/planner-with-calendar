97a3eeb3f84fd473e2401858d77e74bbf5c88b67
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-17 19:46:14 -0300

    add icon to error fallback and change its style

--- src/components/ErrorFallback/ErrorFallback.tsx
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -1,6 +1,12 @@
 type ErrorProps = { error: Error };
 import styles from './_error-fallback.module.scss';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 export const ErrorFallback = ({ error }: ErrorProps) => {
-  return <p className={styles.error}>Something went wrong: {error.message}</p>;
+  return (
+    <div className={styles.error}>
+      <CancelIcon color={'error'} />
+      <p>Something went wrong: {error.message}</p>
+    </div>
+  );
 };
