0df3b347736a65dda4411fc502de6be6b29bc4a0
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-16 23:49:37 -0300

    remove unnecessary param

--- src/components/Calendar/CalendarCells/CalendarCells.tsx
+++ src/components/Calendar/CalendarCells/CalendarCells.tsx
@@ -43,7 +43,6 @@ const CalendarCells = () => {
       year,
       month,
       monthNumberOfDays,
-      false,
     );
     const previousMonthDaysOnCurrentMonth = getPreviousMonthDaysOnCurrentMonth(
       month,
