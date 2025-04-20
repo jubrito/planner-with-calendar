d40fa734242eca9c38d1e3cec7051ebc919da449
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-20 16:31:58 -0300

    wrap home components on error boundary

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,21 @@
-import Calendar from "../../features/Calendar/Calendar";
-import Footer from "../../features/Footer/Footer";
-import Nav from "../../features/Nav/Nav";
-import Planner from "../../features/Planner/Planner";
-import styles from "./_home.module.scss";
+import Calendar from '../../features/Calendar/Calendar';
+import Footer from '../../features/Footer/Footer';
+import Nav from '../../features/Nav/Nav';
+import Planner from '../../features/Planner/Planner';
+import styles from './_home.module.scss';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallback } from '../../components/ErrorFallback/ErrorFallback';
 
 const Home = () => {
   return (
     <main className={styles.home}>
       <Nav />
-      <Calendar />
-      <Planner />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Calendar />
+      </ErrorBoundary>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Planner />
+      </ErrorBoundary>
       <Footer />
     </main>
   );
