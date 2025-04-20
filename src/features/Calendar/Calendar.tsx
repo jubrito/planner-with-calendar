2ec7ec90d2cfa97e2aa820a53c245fc87d41e41d
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-20 16:22:21 -0300

    wrap calendar components on error boundary

--- src/features/Calendar/Calendar.tsx
+++ src/features/Calendar/Calendar.tsx
@@ -1,16 +1,22 @@
-import CalendarCells from "../../components/Calendar/CalendarCells/CalendarCells";
-import { CalendarMenu } from "../../components/Calendar/CalendarMenu/CalendarMenu";
-import CalendarWeeks from "../../components/Calendar/CalendarWeeks/CalendarWeeks";
-import styles from "./_calendar.module.scss";
+import { ErrorBoundary } from 'react-error-boundary';
+import CalendarCells from '../../components/Calendar/CalendarCells/CalendarCells';
+import { CalendarMenu } from '../../components/Calendar/CalendarMenu/CalendarMenu';
+import CalendarWeeks from '../../components/Calendar/CalendarWeeks/CalendarWeeks';
+import styles from './_calendar.module.scss';
+import { ErrorFallback } from '../../components/ErrorFallback/ErrorFallback';
 
 const Calendar = () => {
   return (
     <section className={styles.calendar}>
-      <CalendarMenu />
-      <table aria-labelledby="calendar-month-name">
-        <CalendarWeeks />
-        <CalendarCells />
-      </table>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <CalendarMenu />
+      </ErrorBoundary>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <table aria-labelledby="calendar-month-name">
+          <CalendarWeeks />
+          <CalendarCells />
+        </table>
+      </ErrorBoundary>
     </section>
   );
 };
