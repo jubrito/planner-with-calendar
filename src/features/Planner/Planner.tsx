13b0eb99f91e849b23a4c997ab36c307187b06e4
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-21 02:43:42 -0300

    use current time extracted component

--- src/features/Planner/Planner.tsx
+++ src/features/Planner/Planner.tsx
@@ -4,22 +4,14 @@ import {
   getSelectedDayViewDay,
   getSelectedDayViewDayOfWeek,
   getSelectedDayViewMonthName,
-  getSelectedGlobalDate,
 } from '../../redux/slices/dateSlice/selectors';
 import { getLocaleLanguage } from '../../redux/slices/localeSlice/selectors';
-import {
-  IntlDateTimeFormat2Digit,
-  IntlDateTimeFormatShort,
-} from '../../utils/constants';
+import { IntlDateTimeFormatShort } from '../../utils/constants';
 import { HoursOfTheDay } from './HoursOfTheDay/HoursOfTheDay';
 import { ClickableHoursOfTheDay } from './ClickableHoursOfTheDay/ClickableHoursOfTheDay';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ErrorFallback } from '../../components/ErrorFallback/ErrorFallback';
-import {
-  getFormattedDateString,
-  getTimeInformation,
-  is12HourClockSystem,
-} from '../../utils/calendar/utils';
+import { CurrentTime } from './CurrentTime/CurrentTime';
 
 const Planner = () => {
   const locale = useSelector(getLocaleLanguage());
@@ -29,26 +21,6 @@ const Planner = () => {
   const day = useSelector(getSelectedDayViewDay());
   const dayOfWeek = useSelector(getSelectedDayViewDayOfWeek(locale));
   const plannerDateLabel = `${monthName} ${day}, ${dayOfWeek}`;
-  const date = useSelector(getSelectedGlobalDate());
-  const currentHours = date.getHours();
-  const currentMinutes = date.getMinutes();
-  const fullFormattedCurrentTime = getFormattedDateString(locale, date, {
-    hour: IntlDateTimeFormat2Digit,
-    minute: IntlDateTimeFormat2Digit,
-  });
-  const [currentTime, _currentPeriod, currentHour, currentMins] =
-    getTimeInformation(fullFormattedCurrentTime);
-  const currentTimeDisplay = is12HourClockSystem(fullFormattedCurrentTime)
-    ? `${currentHour}:${currentMins}`
-    : currentTime;
-  const startOfHoursBlockPx = 4;
-  const sizeOfEachHourBlock = 50; // px
-  const oneHour = 60;
-  const sizeOfEachMinute = sizeOfEachHourBlock / oneHour;
-  const top =
-    startOfHoursBlockPx +
-    currentHours * sizeOfEachHourBlock +
-    currentMinutes * sizeOfEachMinute;
 
   return (
     <section className={styles.planner}>
@@ -59,9 +31,7 @@ const Planner = () => {
       </div>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <div className={styles.plannerHours}>
-          <div className={styles.currentHour} style={{ top }}>
-            <span>{currentTimeDisplay}</span>
-          </div>
+          <CurrentTime />
           <HoursOfTheDay />
           <ClickableHoursOfTheDay />
         </div>
