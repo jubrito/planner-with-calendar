03be079104e8233bc033ae2f4d1713f9526afe4f
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-21 16:13:50 -0300

    test: (24-hour format) should correctly display current hour (12:12) for hour 12

--- src/features/Planner/Planner.spec.tsx
+++ src/features/Planner/Planner.spec.tsx
@@ -332,6 +332,48 @@ describe('Planner', () => {
         expect(timeElement).toHaveRole('time');
         expect(timeElement).toHaveProperty('dateTime', time);
       });
+      it('should correctly display current hour (12:12) for hour 12', () => {
+        const hour = 12;
+        const minutes = 12;
+        renderWithProviders(<Planner />, {
+          preloadedState: {
+            dateSlice: {
+              ...initialDateValue,
+              currentState: {
+                ...initialDateValue.currentState,
+                dayViewISODate: new Date(
+                  currentYear,
+                  currentMonth,
+                  currentDay,
+                  hour,
+                  minutes,
+                ).toISOString(),
+                globalISODate: new Date(
+                  currentYear,
+                  currentMonth,
+                  currentDay,
+                  hour,
+                  minutes,
+                ).toISOString(),
+              },
+            },
+            localeSlice: {
+              ...initialLocaleValue,
+              currentState: {
+                ...initialLocaleValue.currentState,
+                locale: {
+                  lang: ptBrLang,
+                },
+              },
+            },
+          },
+        });
+        const time = '12:12';
+        const timeElement = screen.getByText(time);
+        expect(timeElement).toBeInTheDocument();
+        expect(timeElement).toHaveRole('time');
+        expect(timeElement).toHaveProperty('dateTime', time);
+      });
     });
   });
 });
