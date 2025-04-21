7a87c7d2229a673f2cdd10c4966c275d3756cc9c
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-21 19:51:09 -0300

    create EventDetailsModal component

---
+++ src/components/Planner/ClickableHoursOfTheDay/EventDetailsModal/EventDetailsModal.tsx
@@ -0,0 +1,39 @@
+import styles from './event-details-modal.module.scss';
+import CloseIcon from '@mui/icons-material/Close';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+type EventDetailsModalProps = {
+  isOpen: boolean;
+};
+
+export const EventDetailsModal = ({ isOpen }: EventDetailsModalProps) => {
+  if (!isOpen) return <></>;
+
+  return (
+    <div
+      className={styles.modal}
+      id="event-details-modal"
+      onMouseDown={(e) => e.stopPropagation()}
+    >
+      <div className={styles.actions}>
+        <button className="closeTODO" aria-label="Close">
+          <CloseIcon />
+        </button>
+        <button className="deletTODO" aria-label="Delete event">
+          <DeleteIcon />
+        </button>
+        <button
+          className="editTODO"
+          aria-label="Edit event (from X AM to Y AM?)"
+        >
+          <EditIcon />
+        </button>
+      </div>
+      <div className={styles.content}>
+        <p>Title</p>
+        <p>Monday, April 21 ⋅ 10:45 – 11 AM</p>
+      </div>
+    </div>
+  );
+};
