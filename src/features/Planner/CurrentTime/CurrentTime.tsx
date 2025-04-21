5e6cad091291eb7f24ed27353f138ec32ffc7ec6
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-21 02:43:32 -0300

    extract current time component

---
+++ src/features/Planner/CurrentTime/CurrentTime.tsx
@@ -0,0 +1,39 @@
+import { useSelector } from 'react-redux';
+import { getSelectedGlobalDate } from '../../../redux/slices/dateSlice/selectors';
+import { getLocaleLanguage } from '../../../redux/slices/localeSlice/selectors';
+import { IntlDateTimeFormat2Digit } from '../../../utils/constants';
+import {
+  getFormattedDateString,
+  getTimeInformation,
+  is12HourClockSystem,
+} from '../../../utils/calendar/utils';
+import styles from './current-time.module.scss';
+
+export const CurrentTime = () => {
+  const locale = useSelector(getLocaleLanguage());
+  const date = useSelector(getSelectedGlobalDate());
+  const currentHours = date.getHours();
+  const currentMinutes = date.getMinutes();
+  const fullFormattedCurrentTime = getFormattedDateString(locale, date, {
+    hour: IntlDateTimeFormat2Digit,
+    minute: IntlDateTimeFormat2Digit,
+  });
+  const [currentTime, _currentPeriod, currentHour, currentMins] =
+    getTimeInformation(fullFormattedCurrentTime);
+  const currentTimeDisplay = is12HourClockSystem(fullFormattedCurrentTime)
+    ? `${currentHour}:${currentMins}`
+    : currentTime;
+  const startOfHoursBlockPx = 4;
+  const sizeOfEachHourBlock = 50; // px
+  const oneHour = 60;
+  const sizeOfEachMinute = sizeOfEachHourBlock / oneHour;
+  const top =
+    startOfHoursBlockPx +
+    currentHours * sizeOfEachHourBlock +
+    currentMinutes * sizeOfEachMinute;
+  return (
+    <div className={styles.currentHour} style={{ top }}>
+      <span>{currentTimeDisplay}</span>
+    </div>
+  );
+};
