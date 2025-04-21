b23b6a93954412cc68dbba041764a8fc1a6bc682
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-21 17:02:07 -0300

    fix: pass event props to test should display time correctly when event start and end is within the same period

--- src/features/Planner/ClickableHoursOfTheDay/Event/Event.spec.tsx
+++ src/features/Planner/ClickableHoursOfTheDay/Event/Event.spec.tsx
@@ -109,19 +109,30 @@ describe('Event', () => {
   describe('Hours display when using 24-hour clock system', () => {
     const ptBrLang = 'pt-BR';
     it('should display time correctly when event start and end is within the same period', () => {
-      renderWithProviders(<Event event={event} />, {
-        preloadedState: {
-          localeSlice: {
-            ...initialValue,
-            currentState: {
-              ...initialValue.currentState,
-              locale: {
-                lang: ptBrLang,
+      renderWithProviders(
+        <Event
+          event={event}
+          id={event.eventId}
+          title={event.title}
+          startY={event.start.fixedPositionY}
+          endY={event.end.fixedPositionY}
+          startDate={event.start.date}
+          endDate={event.end.date}
+        />,
+        {
+          preloadedState: {
+            localeSlice: {
+              ...initialValue,
+              currentState: {
+                ...initialValue.currentState,
+                locale: {
+                  lang: ptBrLang,
+                },
               },
             },
           },
         },
-      });
+      );
       const eventTimeRange = screen.getByLabelText(
         `Time range from 00:00 to 01:00`,
       );
