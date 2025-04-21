fe00401198d3fcc0f652a0690de2a3904d4969ba
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-21 03:00:58 -0300

    should render current hour correctly for 24-clock systems

--- src/features/Planner/CurrentTime/CurrentTime.spec.tsx
+++ src/features/Planner/CurrentTime/CurrentTime.spec.tsx
@@ -3,6 +3,7 @@ import { screen } from '@testing-library/react';
 import { CurrentTime } from './CurrentTime';
 import { renderWithProviders } from '../../../utils/tests/renderWithProviders';
 import { initialValue } from '../../../redux/slices/dateSlice';
+import { initialValue as localeInitialValue } from '../../../redux/slices/localeSlice';
 import { Months } from '../../../types/calendar/enums';
 
 describe('CurrentTime', () => {
@@ -30,6 +31,39 @@ describe('CurrentTime', () => {
       },
     });
     const time = '03:25';
+    const timeElement = screen.getByText(time);
+    expect(timeElement).toBeInTheDocument();
+    expect(timeElement).toHaveRole('time');
+    expect(timeElement).toHaveProperty('dateTime', time);
+  });
+  it('should render current hour correctly for 24-clock systems', () => {
+    renderWithProviders(<CurrentTime />, {
+      preloadedState: {
+        localeSlice: {
+          ...localeInitialValue,
+          currentState: {
+            ...localeInitialValue.currentState,
+            locale: {
+              lang: 'pt-BR',
+            },
+          },
+        },
+        dateSlice: {
+          ...initialValue,
+          currentState: {
+            ...initialValue.currentState,
+            globalISODate: new Date(
+              year,
+              month,
+              day,
+              hour,
+              minutes,
+            ).toISOString(),
+          },
+        },
+      },
+    });
+    const time = '15:25';
     const timeElement = screen.getByText(time);
     expect(timeElement).toBeInTheDocument();
     expect(timeElement).toHaveRole('time');
