551a7755ac2202b6e3715080736ecb4b588772aa
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-21 20:42:35 -0300

    wrap modal on memo as parent updates frequently

--- src/components/Planner/ClickableHoursOfTheDay/EventDetailsModal/EventDetailsModal.tsx
+++ src/components/Planner/ClickableHoursOfTheDay/EventDetailsModal/EventDetailsModal.tsx
@@ -2,38 +2,38 @@ import styles from './event-details-modal.module.scss';
 import CloseIcon from '@mui/icons-material/Close';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import { memo } from 'react';
 
 type EventDetailsModalProps = {
   top?: number;
   toggleDetailsModal: (eventEndY?: number) => void;
 };
 
-export const EventDetailsModal = ({
-  top = 0,
-  toggleDetailsModal,
-}: EventDetailsModalProps) => {
-  return (
-    <div
-      className={styles.modal}
-      id="event-details-modal"
-      onMouseDown={(e) => e.stopPropagation()}
-      style={{ top }}
-    >
-      <div className={styles.actions}>
-        <button onClick={() => toggleDetailsModal()} aria-label="Close">
-          <CloseIcon />
-        </button>
-        <button aria-label="Delete event">
-          <DeleteIcon />
-        </button>
-        <button aria-label="Edit event (from X AM to Y AM?)">
-          <EditIcon />
-        </button>
+export const EventDetailsModal = memo(
+  ({ top = 0, toggleDetailsModal }: EventDetailsModalProps) => {
+    return (
+      <div
+        className={styles.modal}
+        id="event-details-modal"
+        onMouseDown={(e) => e.stopPropagation()}
+        style={{ top }}
+      >
+        <div className={styles.actions}>
+          <button onClick={() => toggleDetailsModal()} aria-label="Close">
+            <CloseIcon />
+          </button>
+          <button aria-label="Delete event">
+            <DeleteIcon />
+          </button>
+          <button aria-label="Edit event (from X AM to Y AM?)">
+            <EditIcon />
+          </button>
+        </div>
+        <div className={styles.content}>
+          <p>Title</p>
+          <p>Monday, April 21 ⋅ 10:45 – 11 AM</p>
+        </div>
       </div>
-      <div className={styles.content}>
-        <p>Title</p>
-        <p>Monday, April 21 ⋅ 10:45 – 11 AM</p>
-      </div>
-    </div>
-  );
-};
+    );
+  },
+);
