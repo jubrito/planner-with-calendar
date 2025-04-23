ce7f1841634ea960575dca3fda9ad2d39180e66a
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-23 00:06:32 -0300

    should render modal title

--- src/components/Planner/ClickableHoursOfTheDay/EventDetailsModal/EventDetailsModal.spec.tsx
+++ src/components/Planner/ClickableHoursOfTheDay/EventDetailsModal/EventDetailsModal.spec.tsx
@@ -1,3 +1,37 @@
+import { Months } from '../../../../types/calendar/enums';
+import { renderWithProviders } from '../../../../utils/tests/renderWithProviders';
+import { EventDetailsModal } from './EventDetailsModal';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 describe('EventDetailsModal', () => {
-  it('should render modal', () => {});
+  const title = 'title';
+  const year = 2025;
+  const month = Months.FEBRUARY;
+  const day = 11;
+  const hour = 1;
+  const minutes = 59;
+  const startDate = new Date(year, month, day, hour, minutes);
+  const endDate = new Date(year, month, day, hour, minutes);
+  const toggleDetailsModalMock = jest.fn();
+
+  beforeEach(() => {
+    renderWithProviders(
+      <EventDetailsModal
+        startDate={startDate}
+        endDate={endDate}
+        title={title}
+        toggleDetailsModal={toggleDetailsModalMock}
+      />,
+    );
+  });
+
+  it('should render modal title', () => {
+    expect(screen.getByText(title)).toBeInTheDocument();
+  });
+
+  // it('should render modal with same day event', () => {
+  //   const titleText = screen.getByText(title)
+  //   expect()
+  // });
 });
