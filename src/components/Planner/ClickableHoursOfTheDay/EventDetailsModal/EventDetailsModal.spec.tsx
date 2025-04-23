41814c3c90c335bc1fc4c8704938da8d93cf8d8a
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-23 01:36:12 -0300

    should render modal with same day event within same period

--- src/components/Planner/ClickableHoursOfTheDay/EventDetailsModal/EventDetailsModal.spec.tsx
+++ src/components/Planner/ClickableHoursOfTheDay/EventDetailsModal/EventDetailsModal.spec.tsx
@@ -3,16 +3,22 @@ import { renderWithProviders } from '../../../../utils/tests/renderWithProviders
 import { EventDetailsModal } from './EventDetailsModal';
 import { screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { get2DigitsValue } from '../../../../utils/calendar/utils';
 
 describe('EventDetailsModal', () => {
   const title = 'title';
   const year = 2025;
   const month = Months.FEBRUARY;
   const day = 11;
-  const hour = 1;
-  const minutes = 59;
-  const startDate = new Date(year, month, day, hour, minutes);
-  const endDate = new Date(year, month, day, hour, minutes);
+  const startHour = 1;
+  const startMinutes = 59;
+  const endHour = 11;
+  const endMinutes = 59;
+  const weekDay = 'Tue';
+  const monthName = 'Feb';
+  const startDate = new Date(year, month, day, startHour, startMinutes);
+  const endDate = new Date(year, month, day, endHour, endMinutes);
+  const startPeriod = 'AM';
   const toggleDetailsModalMock = jest.fn();
 
   beforeEach(() => {
@@ -30,8 +36,15 @@ describe('EventDetailsModal', () => {
     expect(screen.getByText(title)).toBeInTheDocument();
   });
 
-  // it('should render modal with same day event', () => {
-  //   const titleText = screen.getByText(title)
-  //   expect()
-  // });
+  it('should render modal with same day event within same period', () => {
+    const date = `${weekDay}, ${monthName} ${day}`;
+    const separator = '⋅';
+    const startTime = `${get2DigitsValue(startDate.getHours())}:${get2DigitsValue(startDate.getMinutes())}`;
+    const endTime = `${get2DigitsValue(endDate.getHours())}:${get2DigitsValue(endDate.getMinutes())}`;
+    const fullTime = `${date} ${separator} ${startTime} – ${endTime} ${startPeriod}`;
+    const timeElement = screen.getByText(fullTime);
+    const title = `Event on ${date} from ${startTime} to ${endTime} ${startPeriod}`;
+    expect(timeElement).toBeInTheDocument();
+    expect(timeElement).toHaveProperty('title', title);
+  });
 });
