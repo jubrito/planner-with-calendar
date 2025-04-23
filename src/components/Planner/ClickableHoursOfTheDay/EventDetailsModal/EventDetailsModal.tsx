b9a5961e8673f46f95f8688188969015eb638ed0
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-22 23:16:38 -0300

    get correct event hour on modal

--- src/components/Planner/ClickableHoursOfTheDay/EventDetailsModal/EventDetailsModal.tsx
+++ src/components/Planner/ClickableHoursOfTheDay/EventDetailsModal/EventDetailsModal.tsx
@@ -4,14 +4,99 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { memo } from 'react';
 import { EventOnEdit } from '../ClickableHoursOfTheDay';
+import { getWeekDayName } from '../../../../utils/calendar/weeks';
+import {
+  getFormattedDateString,
+  getMonthIndex,
+  getMonthName,
+  getTimeInformation,
+} from '../../../../utils/calendar/utils';
+import { useSelector } from 'react-redux';
+import { getLocaleLanguage } from '../../../../redux/slices/localeSlice/selectors';
+import {
+  IntlDateTimeFormat2Digit,
+  IntlDateTimeFormatShort,
+} from '../../../../utils/constants';
 
 type EventDetailsModalProps = {
-  top?: number;
   toggleDetailsModal: (eventOnEdit?: EventOnEdit) => void;
+  top?: number;
+  title: EventOnEdit['title'];
+  startDate: EventOnEdit['startDate'];
+  endDate: EventOnEdit['endDate'];
 };
 
 export const EventDetailsModal = memo(
-  ({ top = 0, toggleDetailsModal }: EventDetailsModalProps) => {
+  ({
+    top = 0,
+    toggleDetailsModal,
+    title,
+    startDate,
+    endDate,
+  }: EventDetailsModalProps) => {
+    const locale = useSelector(getLocaleLanguage());
+
+    const getEventTimeDetails = () => {
+      const startDay = startDate.getDate();
+      const startYear = startDate.getFullYear();
+      const startHour = startDate.getHours();
+      const startMinutes = startDate.getMinutes();
+      const startMonthName = getMonthName(
+        locale,
+        startDate,
+        IntlDateTimeFormatShort,
+      );
+
+      const endDay = endDate.getDate();
+      const endYear = startDate.getFullYear();
+      const endHour = endDate.getHours();
+      const endMinutes = endDate.getMinutes();
+      const endMonthName = getMonthName(
+        locale,
+        endDate,
+        IntlDateTimeFormatShort,
+      );
+      const isSameDayEvent =
+        startDay === endDay &&
+        startMonthName === endMonthName &&
+        startYear === endYear;
+      const isSameYear = startYear === endYear;
+      console.log('startDate', startDate);
+      const startYearUpdated = !isSameYear ? `${startYear}, ` : '';
+      const startMonth = getMonthIndex(locale, startDate);
+      const endYearUpdated = !isSameYear ? `${endYear}, ` : '';
+      const endMonth = getMonthIndex(locale, endDate);
+      const startFullTime = getFormattedDateString(
+        locale,
+        new Date(startYear, startMonth, startDay, startHour, startMinutes),
+        {
+          hour: IntlDateTimeFormat2Digit,
+          minute: IntlDateTimeFormat2Digit,
+        },
+      );
+      const endFullTime = getFormattedDateString(
+        locale,
+        new Date(endYear, endMonth, endDay, endHour, endMinutes),
+        {
+          hour: IntlDateTimeFormat2Digit,
+          minute: IntlDateTimeFormat2Digit,
+        },
+      );
+      const [startTime, startPeriod] = getTimeInformation(startFullTime);
+      const [endTime, endPeriod] = getTimeInformation(endFullTime);
+      const updatedStartPeriod = startPeriod === endPeriod ? '' : startPeriod;
+
+      if (isSameDayEvent) {
+        const weekDay = getWeekDayName(startYear, startMonth, startDay, locale);
+        return `${weekDay}, ${startMonthName} ${startDay} ⋅ ${startTime}${updatedStartPeriod} – ${endTime}${endPeriod}`;
+      }
+      return `${startMonthName} ${startDay}, ${startYearUpdated} ${startTime}${startPeriod} – ${endMonthName} ${endDay}, ${endYearUpdated} ${endTime}${endPeriod}`;
+
+      {
+        /* April 21, 2025, 1:30pm – April 22, 2025, 2:45pm */
+      }
+    };
+
     return (
       <div
         className={styles.modal}
@@ -31,8 +116,10 @@ export const EventDetailsModal = memo(
           </button>
         </div>
         <div className={styles.content}>
-          <p>Title</p>
-          <p>Monday, April 21 ⋅ 10:45 – 11 AM</p>
+          <p>{title}</p>
+          <p title={getEventTimeDetails()}>{getEventTimeDetails()}</p>
+          {/* <p>Monday, April 21 ⋅ 10:45 – 11 AM</p> */}
+          {/* April 21, 2025, 1:30pm – April 22, 2025, 2:45pm */}
         </div>
       </div>
     );
