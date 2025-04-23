425d6b6ff09ecf3018c0f3b98acccec63d03dc8b
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-22 23:54:53 -0300

    refactor for readability

--- src/components/Planner/ClickableHoursOfTheDay/EventDetailsModal/EventDetailsModal.tsx
+++ src/components/Planner/ClickableHoursOfTheDay/EventDetailsModal/EventDetailsModal.tsx
@@ -17,6 +17,8 @@ import {
   IntlDateTimeFormat2Digit,
   IntlDateTimeFormatShort,
 } from '../../../../utils/constants';
+import { LocaleLanguage } from '../../../../types/locale/types';
+import { DateConfig } from '../../../../types/calendar/types';
 
 type EventDetailsModalProps = {
   toggleDetailsModal: (eventOnEdit?: EventOnEdit) => void;
@@ -24,6 +26,19 @@ type EventDetailsModalProps = {
   title: EventOnEdit['title'];
   startDate: EventOnEdit['startDate'];
   endDate: EventOnEdit['endDate'];
+};
+
+type EventInfo = {
+  year: DateConfig['year'];
+  month: DateConfig['month'];
+  day: DateConfig['day'];
+  hour: number;
+  minutes: number;
+  monthName: string;
+  formattedFullTime: string;
+  time: string;
+  period: string;
+  weekDay: string;
 };
 
 export const EventDetailsModal = memo(
@@ -37,64 +52,12 @@ export const EventDetailsModal = memo(
     const locale = useSelector(getLocaleLanguage());
 
     const getEventTimeDetails = () => {
-      const startDay = startDate.getDate();
-      const startYear = startDate.getFullYear();
-      const startHour = startDate.getHours();
-      const startMinutes = startDate.getMinutes();
-      const startMonthName = getMonthName(
-        locale,
-        startDate,
-        IntlDateTimeFormatShort,
-      );
+      const startEvent = getEventInfo(startDate, locale);
+      const endEvent = getEventInfo(endDate, locale);
+      const isSameDay = isSameDayEvent(startEvent, endEvent);
 
-      const endDay = endDate.getDate();
-      const endYear = startDate.getFullYear();
-      const endHour = endDate.getHours();
-      const endMinutes = endDate.getMinutes();
-      const endMonthName = getMonthName(
-        locale,
-        endDate,
-        IntlDateTimeFormatShort,
-      );
-      const isSameDayEvent =
-        startDay === endDay &&
-        startMonthName === endMonthName &&
-        startYear === endYear;
-      const isSameYear = startYear === endYear;
-      console.log('startDate', startDate);
-      const startYearUpdated = !isSameYear ? `${startYear}, ` : '';
-      const startMonth = getMonthIndex(locale, startDate);
-      const endYearUpdated = !isSameYear ? `${endYear}, ` : '';
-      const endMonth = getMonthIndex(locale, endDate);
-      const startFullTime = getFormattedDateString(
-        locale,
-        new Date(startYear, startMonth, startDay, startHour, startMinutes),
-        {
-          hour: IntlDateTimeFormat2Digit,
-          minute: IntlDateTimeFormat2Digit,
-        },
-      );
-      const endFullTime = getFormattedDateString(
-        locale,
-        new Date(endYear, endMonth, endDay, endHour, endMinutes),
-        {
-          hour: IntlDateTimeFormat2Digit,
-          minute: IntlDateTimeFormat2Digit,
-        },
-      );
-      const [startTime, startPeriod] = getTimeInformation(startFullTime);
-      const [endTime, endPeriod] = getTimeInformation(endFullTime);
-      const updatedStartPeriod = startPeriod === endPeriod ? '' : startPeriod;
-
-      if (isSameDayEvent) {
-        const weekDay = getWeekDayName(startYear, startMonth, startDay, locale);
-        return `${weekDay}, ${startMonthName} ${startDay} ⋅ ${startTime}${updatedStartPeriod} – ${endTime}${endPeriod}`;
-      }
-      return `${startMonthName} ${startDay}, ${startYearUpdated} ${startTime}${startPeriod} – ${endMonthName} ${endDay}, ${endYearUpdated} ${endTime}${endPeriod}`;
-
-      {
-        /* April 21, 2025, 1:30pm – April 22, 2025, 2:45pm */
-      }
+      if (isSameDay) return formatSameDayEvent(startEvent, endEvent);
+      return formatMultiDayEvent(startEvent, endEvent);
     };
 
     return (
@@ -118,10 +81,56 @@ export const EventDetailsModal = memo(
         <div className={styles.content}>
           <p>{title}</p>
           <p title={getEventTimeDetails()}>{getEventTimeDetails()}</p>
-          {/* <p>Monday, April 21 ⋅ 10:45 – 11 AM</p> */}
-          {/* April 21, 2025, 1:30pm – April 22, 2025, 2:45pm */}
         </div>
       </div>
     );
   },
 );
+
+const getEventInfo = (date: Date, locale: LocaleLanguage): EventInfo => {
+  const year = date.getFullYear();
+  const month = getMonthIndex(locale, date);
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const monthName = getMonthName(locale, date, IntlDateTimeFormatShort);
+  const formattedFullTime = getFormattedFullTime(date, locale);
+  const [time, period] = getTimeInformation(formattedFullTime);
+  const weekDay = getWeekDayName(year, month, day, locale);
+  return {
+    year,
+    month,
+    monthName,
+    day,
+    hour,
+    minutes,
+    formattedFullTime,
+    time,
+    period,
+    weekDay,
+  };
+};
+
+const getFormattedFullTime = (date: Date, locale: LocaleLanguage) =>
+  getFormattedDateString(locale, date, {
+    hour: IntlDateTimeFormat2Digit,
+    minute: IntlDateTimeFormat2Digit,
+  });
+
+const isSameDayEvent = (startEvent: EventInfo, endEvent: EventInfo) =>
+  startEvent.day === endEvent.day &&
+  startEvent.monthName === endEvent.monthName &&
+  startEvent.year === endEvent.year;
+
+const formatSameDayEvent = (startEvent: EventInfo, endEvent: EventInfo) => {
+  const updatedStartPeriod =
+    startEvent.period === endEvent.period ? '' : startEvent.period;
+  return `${startEvent.weekDay}, ${startEvent.monthName} ${startEvent.day} ⋅ ${startEvent.time}${updatedStartPeriod} – ${endEvent.time}${endEvent.period}`;
+};
+
+const formatMultiDayEvent = (startEvent: EventInfo, endEvent: EventInfo) => {
+  const isSameYearEvent = startEvent.year === startEvent.year;
+  const startYearUpdated = !isSameYearEvent ? `${startEvent.year}, ` : '';
+  const endYearUpdated = !isSameYearEvent ? `${endEvent.year}, ` : '';
+  return `${startEvent.monthName} ${startEvent.day}, ${startYearUpdated} ${startEvent.time}${startEvent.period} – ${endEvent.monthName} ${endEvent.day}, ${endYearUpdated} ${endEvent.time}${endEvent.period}`;
+};
