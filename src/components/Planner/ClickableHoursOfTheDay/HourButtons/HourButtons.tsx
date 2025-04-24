a63a4e4510dcee1f603734275b0e3b67f333d10b
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-24 00:32:41 -0300

    refactor hour buttons component

--- src/components/Planner/ClickableHoursOfTheDay/HourButtons/HourButtons.tsx
+++ src/components/Planner/ClickableHoursOfTheDay/HourButtons/HourButtons.tsx
@@ -13,42 +13,20 @@ import {
 } from '../../../../redux/slices/dateSlice/selectors';
 import { IntlDateTimeFormat2Digit } from '../../../../utils/constants';
 import styles from './hour-buttons.module.scss';
+import { LocaleLanguage } from '../../../../types/locale/types';
+import { DateConfig } from '../../../../types/calendar/types';
 
 export const HourButtons = () => {
   const twentyFourHours = Array.from(Array(numberOfHoursInADay).keys());
-  const localeString = useSelector(getLocaleLanguage());
+  const locale = useSelector(getLocaleLanguage());
   const year = useSelector(getSelectedGlobalYear());
-  const month = useSelector(getSelectedGlobalMonth(localeString));
+  const month = useSelector(getSelectedGlobalMonth(locale));
   const day = useSelector(getSelectedGlobalDay());
 
   return (
     <>
       {twentyFourHours.map((index) => {
-        const startFullHour = getFormattedDateString(
-          localeString,
-          new Date(year, month, day, index),
-          {
-            hour: IntlDateTimeFormat2Digit,
-            minute: IntlDateTimeFormat2Digit,
-          },
-        );
-        const endFullHour = getFormattedDateString(
-          localeString,
-          new Date(year, month, day, index + 1),
-          {
-            hour: IntlDateTimeFormat2Digit,
-            minute: IntlDateTimeFormat2Digit,
-          },
-        );
-        const [startTime, startPeriod, startHour] =
-          getTimeInformation(startFullHour);
-        const [endTime, endPeriod, endHour] = getTimeInformation(endFullHour);
-        const updatedStartPeriod = startPeriod !== endPeriod ? startPeriod : '';
-        const updatedStart = is12HourClockSystem(startFullHour)
-          ? startHour
-          : startTime;
-        const updatedEnd = is12HourClockSystem(endFullHour) ? endHour : endTime;
-        const range = `${updatedStart}${updatedStartPeriod} to ${updatedEnd}${endPeriod}`;
+        const range = getHourRange(locale, year, month, day, index);
         const title = `Click, hold, and drag to create an event within ${range}`;
         return (
           <div
@@ -61,4 +39,37 @@ export const HourButtons = () => {
       })}
     </>
   );
+};
+
+const getHourRange = (
+  locale: LocaleLanguage,
+  year: DateConfig['year'],
+  month: DateConfig['month'],
+  day: DateConfig['day'],
+  index: number,
+) => {
+  const startFullHour = getFormattedDateString(
+    locale,
+    new Date(year, month, day, index),
+    {
+      hour: IntlDateTimeFormat2Digit,
+      minute: IntlDateTimeFormat2Digit,
+    },
+  );
+  const endFullHour = getFormattedDateString(
+    locale,
+    new Date(year, month, day, index + 1),
+    {
+      hour: IntlDateTimeFormat2Digit,
+      minute: IntlDateTimeFormat2Digit,
+    },
+  );
+  const [startTime, startPeriod, startHour] = getTimeInformation(startFullHour);
+  const [endTime, endPeriod, endHour] = getTimeInformation(endFullHour);
+  const updatedStartPeriod = startPeriod !== endPeriod ? startPeriod : '';
+  const updatedStart = is12HourClockSystem(startFullHour)
+    ? startHour
+    : startTime;
+  const updatedEnd = is12HourClockSystem(endFullHour) ? endHour : endTime;
+  return `${updatedStart}${updatedStartPeriod} to ${updatedEnd}${endPeriod}`;
 };
