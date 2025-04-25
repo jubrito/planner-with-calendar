89058315c7023d6a3ee0c7cb943b2fbade0ebcf9
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-24 22:59:31 -0300

    fix: correctly get current event from current state

--- src/redux/slices/eventSlice/selectors.ts
+++ src/redux/slices/eventSlice/selectors.ts
@@ -7,7 +7,7 @@ export const getInitialEvent = () =>
   createSelector(eventSlice, (state) => state.initialState.event);
 
 export const getCurrentEvent = () =>
-  createSelector(eventSlice, (state) => state.initialState.event);
+  createSelector(eventSlice, (state) => state.currentState.event);
 
 export const getInitialEvents = () =>
   createSelector(eventSlice, (state) => state.initialState.allEvents);
