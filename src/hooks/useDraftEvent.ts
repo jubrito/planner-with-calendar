dd71d7df2896962bf7e81c622dc24222ed35c2a7
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-25 00:49:36 -0300

    create createDraftEvent inside d

---
+++ src/hooks/useDraftEvent.ts
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { EventBlock } from '../components/Planner/ClickableHoursOfTheDay/ClickableHoursOfTheDay';
+import { getFixedRelativeY } from '../components/Planner/ClickableHoursOfTheDay/getPositionsY';
+import {
+  getEndBlock,
+  getStartBlock,
+} from '../components/Planner/ClickableHoursOfTheDay/getBlocks';
+import {
+  getSelectedDayViewDay,
+  getSelectedDayViewMonth,
+  getSelectedDayViewYear,
+} from '../redux/slices/dateSlice/selectors';
+import { useSelector } from 'react-redux';
+import { getLocaleLanguage } from '../redux/slices/localeSlice/selectors';
+
+export const useDraftEvent = () => {
+  const locale = useSelector(getLocaleLanguage());
+  const year = useSelector(getSelectedDayViewYear());
+  const month = useSelector(getSelectedDayViewMonth(locale));
+  const day = useSelector(getSelectedDayViewDay());
+  const [draftEvent, setDraftEvent] = useState<EventBlock | null>(null);
+
+  const createDraftEvent = (relativeY: number) => {
+    const startBlock = getStartBlock(relativeY);
+    const endBlock = getEndBlock(startBlock);
+    const fixedHourAnd15MinBlock = getFixedRelativeY(startBlock, 'start');
+    setDraftEvent({
+      title: '(No title)',
+      eventId: `draft-${Date.now()}`,
+      start: {
+        fixedPositionY: fixedHourAnd15MinBlock,
+        block: startBlock,
+        date: new Date(year, month, day, startBlock.hour, startBlock.minutes),
+      },
+      end: {
+        fixedPositionY: fixedHourAnd15MinBlock,
+        block: endBlock,
+        date: new Date(year, month, day, endBlock.hour, endBlock.minutes),
+      },
+    });
+  };
+
+  return {
+    draftEvent,
+    createDraftEvent,
+  };
+};
