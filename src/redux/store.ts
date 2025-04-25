7c01f024de66acb80184f653f2d3312aa35e1487
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-24 22:57:43 -0300

    add event slice to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import dateReducer from "./slices/dateSlice";
-import localeReducer from "./slices/localeSlice";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import dateReducer from './slices/dateSlice';
+import localeReducer from './slices/localeSlice';
+import eventReducer from './slices/eventSlice';
 
 const rootReducer = combineReducers({
   dateSlice: dateReducer,
   localeSlice: localeReducer,
+  eventSlice: eventReducer,
 });
 
 const setupStore = (preloadedState?: Partial<RootState>) =>
@@ -15,6 +17,6 @@ const setupStore = (preloadedState?: Partial<RootState>) =>
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
-export type AppDispatch = AppStore["dispatch"];
+export type AppDispatch = AppStore['dispatch'];
 
 export default setupStore;
