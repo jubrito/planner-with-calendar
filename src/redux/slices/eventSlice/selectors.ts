e780945cbbce4ee97d697620743eb350c36f3dcf
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-24 23:44:40 -0300

    only exposes events selectors

--- src/redux/slices/eventSlice/selectors.ts
+++ src/redux/slices/eventSlice/selectors.ts
@@ -3,14 +3,8 @@ import { RootState } from '../../store';
 
 const eventSlice = (state: RootState) => state.eventSlice;
 
-export const getInitialSelectedEvent = () =>
-  createSelector(eventSlice, (state) => state.initialState.selectedEvent);
-
-export const getCurrentSelectedEvent = () =>
-  createSelector(eventSlice, (state) => state.currentState.selectedEvent);
-
 export const getInitialEvents = () =>
-  createSelector(eventSlice, (state) => state.initialState.allEvents);
+  createSelector(eventSlice, (state) => state.initialState.events);
 
 export const getCurrentEvents = () =>
-  createSelector(eventSlice, (state) => state.currentState.allEvents);
+  createSelector(eventSlice, (state) => state.currentState.events);
