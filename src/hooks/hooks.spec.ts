7d3117ed6d833c06ae5be169aacc07cc1e27d60f
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-29 23:55:57 -0300

    should return updated draft event when creating event

--- src/hooks/hooks.spec.ts
+++ src/hooks/hooks.spec.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { Months } from '../types/calendar/enums';
 
 import { useEvent } from './useDraftEvent';
@@ -11,16 +11,41 @@ describe('React hooks', () => {
 
     describe('Only passing locale to constructor', () => {
       it('should return draft event initially as undefined', () => {
-        // const currentDate = new Date();
         const { result } = renderHook(() => useEvent(year, month, day));
-        const {
-          draftEvent,
-          // createDraftEvent,
-          // updateDraftEvent,
-          // clearDraftEvent,
-          // createEvent,
-        } = result.current;
+        const { draftEvent } = result.current;
         expect(draftEvent).toBe(undefined);
+      });
+      it('should return updated draft event when creating event', () => {
+        const { result, rerender } = renderHook(() =>
+          useEvent(year, month, day),
+        );
+        const { createDraftEvent } = result.current;
+        const relativeY = 49;
+
+        act(() => {
+          createDraftEvent(relativeY);
+        });
+        rerender();
+
+        const { draftEvent: draftEventUpdated } = result.current;
+        console.log(draftEventUpdated);
+        expect(draftEventUpdated).toBeDefined();
+        expect(draftEventUpdated?.id).toBeDefined();
+        expect(draftEventUpdated?.title).toStrictEqual('(No title)');
+        expect(draftEventUpdated?.start.block).toStrictEqual({
+          fifteenMinBlock: 3,
+          hour: 0,
+          minutes: 45,
+        });
+        expect(draftEventUpdated?.end.block).toStrictEqual({
+          fifteenMinBlock: 4,
+          hour: 1,
+          minutes: 0,
+        });
+        expect(draftEventUpdated?.start.fixedPositionY).toStrictEqual(37.5);
+        expect(draftEventUpdated?.end.fixedPositionY).toStrictEqual(37.5);
+        expect(draftEventUpdated?.start.date).toBeDefined();
+        expect(draftEventUpdated?.end.date).toBeDefined();
       });
       // it('should update and return the date', async () => {
       //   let currentDate = new Date();
