4e564ab5850616b277fb268687004530f6c5faeb
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-05-01 00:27:18 -0300

    wrap existing tests on describe block

--- src/components/Planner/EventsContainer/EventsContainer.spec.tsx
+++ src/components/Planner/EventsContainer/EventsContainer.spec.tsx
@@ -4,76 +4,78 @@ import { renderWithProviders } from '../../../utils/tests/renderWithProviders';
 import { EventContainer } from './EventsContainer';
 
 describe('EventContainer', () => {
-  it('should display event details when interacting with the visible portion of the container', async () => {
-    const { container } = renderWithProviders(<EventContainer />);
-    const targetElement = container.firstElementChild;
-    expect(targetElement).not.toBe(null);
-    if (targetElement) {
-      const rect = targetElement.getBoundingClientRect();
-      const positionY = rect.top;
-      const mouseDownEvent = new MouseEvent('mousedown', {
-        clientY: positionY,
-        bubbles: true,
-      });
-      const mouseMoveEvent = new MouseEvent('mousemove', {
-        clientY: 49,
-        bubbles: true,
-      });
-      const mouseUpEvent = new MouseEvent('mouseup', {
-        clientY: 49,
-        bubbles: true,
-      });
-      act(() => {
-        targetElement.dispatchEvent(mouseDownEvent);
-      });
-      act(() => {
-        targetElement.dispatchEvent(mouseMoveEvent);
-      });
-      act(() => {
-        targetElement.dispatchEvent(mouseUpEvent);
-      });
-      const eventDefaultTitle = '(No title)';
-      const eventTime = '12:00 – 01:00 AM';
-      await waitFor(() => {
-        expect(screen.getByText(eventDefaultTitle)).toBeInTheDocument();
-        expect(screen.getByText(eventTime)).toBeInTheDocument();
-      });
-    }
-  });
-  it('should display event details when interacting with the not visible portion of the container', async () => {
-    const { container } = renderWithProviders(<EventContainer />);
-    const targetElement = container.firstElementChild;
-    expect(targetElement).not.toBe(null);
-    if (targetElement) {
-      const rect = targetElement.getBoundingClientRect();
-      const positionY = rect.top;
-      const mouseDownEvent = new MouseEvent('mousedown', {
-        clientY: positionY + 525,
-        bubbles: true,
-      });
-      const mouseMoveEvent = new MouseEvent('mousemove', {
-        clientY: positionY + 625,
-        bubbles: true,
-      });
-      const mouseUpEvent = new MouseEvent('mouseup', {
-        clientY: positionY + 625,
-        bubbles: true,
-      });
-      act(() => {
-        targetElement.dispatchEvent(mouseDownEvent);
-      });
-      act(() => {
-        targetElement.dispatchEvent(mouseMoveEvent);
-      });
-      act(() => {
-        targetElement.dispatchEvent(mouseUpEvent);
-      });
-      const eventDefaultTitle = '(No title)';
-      const eventTime = '10:30 AM – 12:45 PM';
-      await waitFor(() => {
-        expect(screen.getByText(eventDefaultTitle)).toBeInTheDocument();
-        expect(screen.getByText(eventTime)).toBeInTheDocument();
-      });
-    }
+  describe('When creating an event', () => {
+    it('should display event details when interacting with the visible portion of the container', async () => {
+      const { container } = renderWithProviders(<EventContainer />);
+      const targetElement = container.firstElementChild;
+      expect(targetElement).not.toBe(null);
+      if (targetElement) {
+        const rect = targetElement.getBoundingClientRect();
+        const positionY = rect.top;
+        const mouseDownEvent = new MouseEvent('mousedown', {
+          clientY: positionY,
+          bubbles: true,
+        });
+        const mouseMoveEvent = new MouseEvent('mousemove', {
+          clientY: 49,
+          bubbles: true,
+        });
+        const mouseUpEvent = new MouseEvent('mouseup', {
+          clientY: 49,
+          bubbles: true,
+        });
+        act(() => {
+          targetElement.dispatchEvent(mouseDownEvent);
+        });
+        act(() => {
+          targetElement.dispatchEvent(mouseMoveEvent);
+        });
+        act(() => {
+          targetElement.dispatchEvent(mouseUpEvent);
+        });
+        const eventDefaultTitle = '(No title)';
+        const eventTime = '12:00 – 01:00 AM';
+        await waitFor(() => {
+          expect(screen.getByText(eventDefaultTitle)).toBeInTheDocument();
+          expect(screen.getByText(eventTime)).toBeInTheDocument();
+        });
+      }
+    });
+    it('should display event details when interacting with the not visible portion of the container', async () => {
+      const { container } = renderWithProviders(<EventContainer />);
+      const targetElement = container.firstElementChild;
+      expect(targetElement).not.toBe(null);
+      if (targetElement) {
+        const rect = targetElement.getBoundingClientRect();
+        const positionY = rect.top;
+        const mouseDownEvent = new MouseEvent('mousedown', {
+          clientY: positionY + 525,
+          bubbles: true,
+        });
+        const mouseMoveEvent = new MouseEvent('mousemove', {
+          clientY: positionY + 625,
+          bubbles: true,
+        });
+        const mouseUpEvent = new MouseEvent('mouseup', {
+          clientY: positionY + 625,
+          bubbles: true,
+        });
+        act(() => {
+          targetElement.dispatchEvent(mouseDownEvent);
+        });
+        act(() => {
+          targetElement.dispatchEvent(mouseMoveEvent);
+        });
+        act(() => {
+          targetElement.dispatchEvent(mouseUpEvent);
+        });
+        const eventDefaultTitle = '(No title)';
+        const eventTime = '10:30 AM – 12:45 PM';
+        await waitFor(() => {
+          expect(screen.getByText(eventDefaultTitle)).toBeInTheDocument();
+          expect(screen.getByText(eventTime)).toBeInTheDocument();
+        });
+      }
+    });
   });
 });
