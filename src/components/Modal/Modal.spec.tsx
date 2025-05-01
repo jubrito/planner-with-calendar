87d2080f2483f9800c29a0a0eb57ece37db51fc1
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-04-30 22:08:13 -0300

    test: should render modal title

--- src/components/Modal/Modal.spec.tsx
+++ src/components/Modal/Modal.spec.tsx
@@ -1,1 +1,41 @@
-describe('Modal', () => {});
+import { renderWithProviders } from '../../utils/tests/renderWithProviders';
+import { Modal } from './Modal';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Modal', () => {
+  const eventStyle = {};
+  const eventTitle = 'Title';
+  const closeModalMock = jest.fn();
+  const editModalMock = jest.fn();
+  const deleteModalMock = jest.fn();
+  const closeLabel = 'Close';
+  const editLabel = 'Edit';
+  const deleteLabel = 'Delete';
+  const content = <>content</>;
+
+  beforeEach(() => {
+    renderWithProviders(
+      <Modal
+        content={content}
+        style={eventStyle}
+        title={eventTitle}
+        closeModal={{
+          closeLabel: closeLabel,
+          handleClose: closeModalMock,
+        }}
+        editModal={{
+          editLabel: editLabel,
+          handleEdit: editModalMock,
+        }}
+        deleteModal={{
+          deleteLabel: deleteLabel,
+          handleDelete: deleteModalMock,
+        }}
+      />,
+    );
+  });
+  it('should render modal title', () => {
+    expect(screen.getByText(eventTitle)).toBeInTheDocument();
+  });
+});
