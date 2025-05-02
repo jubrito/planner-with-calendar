097550529348d680ac1526b9b37d580789e028b1
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-05-01 23:43:30 -0300

    fix modals container test

--- src/components/Planner/EventModalsContainer/EventModalsContainer.spec.tsx
+++ src/components/Planner/EventModalsContainer/EventModalsContainer.spec.tsx
@@ -5,36 +5,42 @@ import { EventDetailsModal } from './EventDetailsModal/EventDetailsModal';
 import { initialValue } from '../../../redux/slices/eventSlice';
 
 describe('EventModalsContainer', () => {
+  const refMock = { current: null };
   const eventTitle = 'eventTitle';
   it('should not render modal if selected event is not defined', () => {
-    renderWithProviders(<EventDetailsModal closeModal={jest.fn()} />);
+    renderWithProviders(
+      <EventDetailsModal closeModal={jest.fn()} viewEventModalRef={refMock} />,
+    );
     const event = screen.queryByText(eventTitle);
     expect(event).not.toBeInTheDocument();
   });
   it('should render modal if selected event is defined', () => {
-    renderWithProviders(<EventDetailsModal closeModal={jest.fn()} />, {
-      preloadedState: {
-        eventSlice: {
-          ...initialValue,
-          currentState: {
-            ...initialValue.currentState,
-            selectedDayViewEvent: {
-              event: {
-                dayViewPosition: {
-                  endY: 0,
-                  startY: 0,
+    renderWithProviders(
+      <EventDetailsModal closeModal={jest.fn()} viewEventModalRef={refMock} />,
+      {
+        preloadedState: {
+          eventSlice: {
+            ...initialValue,
+            currentState: {
+              ...initialValue.currentState,
+              selectedDayViewEvent: {
+                event: {
+                  dayViewPosition: {
+                    endY: 0,
+                    startY: 0,
+                  },
+                  endDate: new Date().toISOString(),
+                  startDate: new Date().toISOString(),
+                  id: 'id',
+                  title: eventTitle,
                 },
-                endDate: new Date(),
-                startDate: new Date(),
-                id: 'id',
-                title: eventTitle,
+                top: 0,
               },
-              top: 0,
             },
           },
         },
       },
-    });
+    );
     const event = screen.getByText(eventTitle);
     expect(event).toBeInTheDocument();
   });
