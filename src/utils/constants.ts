b8c36fc688c699407020c6c1453fe9d1922e77fb
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-05-02 00:15:34 -0300

    add enter const

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,4 @@ export const IntlDateTimeFormatNumeric = 'numeric';
 export const IntlDateTimeFormat2Digit = '2-digit';
 export const IntlDateTimeFormatNarrow = 'narrow';
 export const spacingTopClickablePlannerArea = 11; // must be the same as the scss variables.$spacing-top-clickable-planner-area
+export const enterKey = 'Enter';
