2cb16ebf09fe87dfa298d500f5b39deced19d722
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-05-04 22:25:36 -0300

    replace toDateString with getDateISOString reusable function

--- src/features/Planner/components/EventModalsContainer/EventModalsContainer.spec.tsx
+++ src/features/Planner/components/EventModalsContainer/EventModalsContainer.spec.tsx
@@ -3,20 +3,29 @@ import '@testing-library/jest-dom';
 import { EventDetailsModal } from './EventDetailsModal/EventDetailsModal';
 import { renderWithProviders } from '../../../../utils/tests/renderWithProviders';
 import { initialValue } from '../../../../redux/slices/eventSlice';
+import { getDateISOString } from '../../../../utils/calendar/utils';
 
 describe('EventModalsContainer', () => {
   const refMock = { current: null };
   const eventTitle = 'eventTitle';
   it('should not render modal if selected event is not defined', () => {
     renderWithProviders(
-      <EventDetailsModal closeModal={jest.fn()} viewEventModalRef={refMock} />,
+      <EventDetailsModal
+        editModal={jest.fn()}
+        closeModal={jest.fn()}
+        viewEventModalRef={refMock}
+      />,
     );
     const event = screen.queryByText(eventTitle);
     expect(event).not.toBeInTheDocument();
   });
   it('should render modal if selected event is defined', () => {
     renderWithProviders(
-      <EventDetailsModal closeModal={jest.fn()} viewEventModalRef={refMock} />,
+      <EventDetailsModal
+        editModal={jest.fn()}
+        closeModal={jest.fn()}
+        viewEventModalRef={refMock}
+      />,
       {
         preloadedState: {
           eventSlice: {
@@ -25,8 +34,8 @@ describe('EventModalsContainer', () => {
               ...initialValue.currentState,
               selectedDayViewEvent: {
                 event: {
-                  endDate: new Date().toISOString(),
-                  startDate: new Date().toISOString(),
+                  endDate: getDateISOString(new Date()),
+                  startDate: getDateISOString(new Date()),
                   id: 'id',
                   title: eventTitle,
                 },
