7fe8f60e539135d2d28f679592da3196a7aa382a
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-05-18 22:55:34 -0300

    create start and end labels

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,5 +4,7 @@ export const IntlDateTimeFormatShort = 'short';
 export const IntlDateTimeFormatNumeric = 'numeric';
 export const IntlDateTimeFormat2Digit = '2-digit';
 export const IntlDateTimeFormatNarrow = 'narrow';
+export const startLabel = 'start';
+export const endLabel = 'end';
 export const spacingTopClickablePlannerArea = 11; // must be the same as the scss variables.$spacing-top-clickable-planner-area
 export const enterKey = 'Enter';
