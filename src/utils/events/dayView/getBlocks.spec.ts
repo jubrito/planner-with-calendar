6f029ad5fb86b32953e5449e4333d2deb7a91e57
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-05-21 01:12:57 -0300

    test: should return second block if rest is equal to or greater than 0.25 and smaller than 0.50

--- src/utils/events/dayView/getBlocks.spec.ts
+++ src/utils/events/dayView/getBlocks.spec.ts
@@ -9,17 +9,21 @@ describe('getBlocks', () => {
     it('should return first block if rest is negative', () => {
       expect(getFifteenMinuteBlock(-1)).toBe(firstBlock);
     });
-    it('should return first block if rest is greater than or equal to 0 and smaller than 0.25', () => {
+    it('should return first block if rest is equal to or greater than 0 and smaller than 0.25', () => {
       expect(getFifteenMinuteBlock(0)).toBe(firstBlock);
       expect(getFifteenMinuteBlock(0.19)).toBe(firstBlock);
-      expect(getFifteenMinuteBlock(0.24)).toBe(firstBlock);
       expect(getFifteenMinuteBlock(0.2499)).toBe(firstBlock);
       expect(getFifteenMinuteBlock(0.25)).not.toBe(firstBlock);
+    });
+    it('should return second block if rest is equal to or greater than 0.25 and smaller than 0.50', () => {
+      expect(getFifteenMinuteBlock(0.2499)).not.toBe(secondBlock);
+      expect(getFifteenMinuteBlock(0.25)).toBe(secondBlock);
+      expect(getFifteenMinuteBlock(0.4999)).toBe(secondBlock);
+      expect(getFifteenMinuteBlock(0.5)).not.toBe(secondBlock);
     });
     it('should return last block if rest is greater than or equal to 0.75 and smaller than 1', () => {
       expect(getFifteenMinuteBlock(0.75)).toBe(lastBlock);
       expect(getFifteenMinuteBlock(0.75999)).toBe(lastBlock);
-      expect(getFifteenMinuteBlock(0.76)).toBe(lastBlock);
       expect(getFifteenMinuteBlock(0.999)).toBe(lastBlock);
       expect(getFifteenMinuteBlock(1)).not.toBe(lastBlock);
     });
