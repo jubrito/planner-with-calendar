525931326931d7a881c5caf18c9122869819a9f5
Juliana Witzke de Brito <jujubrito@outlook.com>
2025-05-21 00:29:47 -0300

    create weeks mock

---
+++ src/mocks/utils/utils.ts
@@ -0,0 +1,37 @@
+export const weekDaysNamesOnEnglishLongFormat = [
+  {
+    initial: 'M',
+    long: 'Monday',
+    short: 'Mon',
+  },
+  {
+    initial: 'T',
+    long: 'Tuesday',
+    short: 'Tue',
+  },
+  {
+    initial: 'W',
+    long: 'Wednesday',
+    short: 'Wed',
+  },
+  {
+    initial: 'T',
+    long: 'Thursday',
+    short: 'Thu',
+  },
+  {
+    initial: 'F',
+    long: 'Friday',
+    short: 'Fri',
+  },
+  {
+    initial: 'S',
+    long: 'Saturday',
+    short: 'Sat',
+  },
+  {
+    initial: 'S',
+    long: 'Sunday',
+    short: 'Sun',
+  },
+];
