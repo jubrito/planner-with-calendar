072761a1126c2d57533fe7cb7534ded795b44571
jubrito <jujubrito@outlook.com>
2025-06-22 22:17:12 -0300

    test: should render calendar header

---
+++ src/features/Calendar/Calendar.spec.tsx
@@ -0,0 +1,40 @@
+import {
+  getFormattedDateString,
+  getMonthName,
+} from '../../utils/calendar/utils';
+import { getWeekDaysNames } from '../../utils/calendar/weeks';
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/dom';
+import { renderWithProviders } from '../../utils/tests/renderWithProviders';
+import Calendar from './Calendar';
+import { initialValue } from '../../redux/slices/dateSlice';
+
+describe('Calendar', () => {
+  const englishLocale = 'en-us';
+  const date = new Date();
+  const year = date.getFullYear();
+  const ISODate = getFormattedDateString(englishLocale, date);
+
+  beforeEach(() => {
+    renderWithProviders(<Calendar />, {
+      preloadedState: {
+        dateSlice: {
+          ...initialValue,
+          currentState: {
+            ...initialValue.currentState,
+            globalISODate: ISODate,
+            dayViewISODate: ISODate,
+          },
+        },
+      },
+    });
+  });
+
+  it('should render calendar header', () => {
+    const currentMonthName = getMonthName(englishLocale, date);
+
+    expect(
+      screen.getByText(`${currentMonthName}, ${year}`),
+    ).toBeInTheDocument();
+  });
+});
