6aee81f254f04d70fd1d46eddf6e075a4ce1e4b2
jubrito <jujubrito@outlook.com>
2025-06-29 19:26:34 -0300

    extact header component

--- src/features/Planner/Planner.tsx
+++ src/features/Planner/Planner.tsx
@@ -1,37 +1,24 @@
 import { useSelector } from 'react-redux';
 import styles from './_planner.module.scss';
-import {
-  getSelectedDayViewDate,
-  getSelectedDayViewDay,
-  getSelectedDayViewDayOfWeek,
-  getSelectedDayViewMonthName,
-} from '../../redux/slices/dateSlice/selectors';
 import { getLocaleLanguage } from '../../redux/slices/localeSlice/selectors';
-import { IntlDateTimeFormatShort } from '../../utils/constants';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ErrorFallback } from '../../components/ErrorFallback/ErrorFallback';
 import { CurrentTime } from './components/CurrentTime/CurrentTime';
 import { HoursOfTheDay } from './components/HoursOfTheDay/HoursOfTheDay';
 import { EventContainer } from './components/EventsContainer/EventsContainer';
 import { isToday } from '../../utils/checkers';
+import { Header } from './components/Header/Header';
+import { getSelectedDayViewDate } from '../../redux/slices/dateSlice/selectors';
 
 const Planner = () => {
   const locale = useSelector(getLocaleLanguage());
-  const monthName = useSelector(
-    getSelectedDayViewMonthName(locale, IntlDateTimeFormatShort),
-  );
-  const day = useSelector(getSelectedDayViewDay());
   const date = useSelector(getSelectedDayViewDate());
-  const dayOfWeek = useSelector(getSelectedDayViewDayOfWeek(locale));
-  const plannerDateLabel = `${monthName} ${day}, ${dayOfWeek}`;
 
   return (
     <section className={styles.planner}>
-      <div className={styles.plannerHeader}>
-        <h2 className={styles.plannerHeaderLabel} id="calendar-month-name">
-          {plannerDateLabel}
-        </h2>
-      </div>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Header />
+      </ErrorBoundary>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <div className={styles.plannerHours}>
           {isToday(locale, date) && <CurrentTime />}
