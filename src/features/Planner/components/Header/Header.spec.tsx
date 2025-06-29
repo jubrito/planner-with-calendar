15ae691c5edf326ae02ec0f3e66e12f0db2085ae
jubrito <jujubrito@outlook.com>
2025-06-29 19:28:29 -0300

    test: should render header initial date text in English

---
+++ src/features/Planner/components/Header/Header.spec.tsx
@@ -0,0 +1,62 @@
+import { renderWithProviders } from '../../../../utils/tests/renderWithProviders';
+import { initialValue as initialDateValue } from '../../../../redux/slices/dateSlice';
+import { initialValue as initialLocaleValue } from '../../../../redux/slices/localeSlice';
+import { Months } from '../../../../types/calendar/enums';
+import {
+  getDateISOString,
+  getDayOfWeek,
+  getMonthName,
+} from '../../../../utils/calendar/utils';
+import { LocaleLanguage } from '../../../../types/locale/types';
+import { IntlDateTimeFormatShort } from '../../../../utils/constants';
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/dom';
+import { Header } from './Header';
+
+describe('Header', () => {
+  const currentYear = 2025;
+  const currentMonth = Months.DECEMBER;
+  const currentDay = 1;
+  const brLocale = 'pt-BR';
+  const enEsLocale = 'en-US';
+
+  const renderHeader = ({ locale }: { locale?: LocaleLanguage }) => {
+    return renderWithProviders(<Header />, {
+      preloadedState: {
+        dateSlice: {
+          ...initialDateValue,
+          currentState: {
+            ...initialDateValue.currentState,
+            dayViewISODate: getDateISOString(
+              new Date(currentYear, currentMonth, currentDay),
+            ),
+            globalISODate: getDateISOString(
+              new Date(currentYear, currentMonth, currentDay),
+            ),
+          },
+        },
+        localeSlice: {
+          ...initialLocaleValue,
+          currentState: {
+            ...initialLocaleValue.currentState,
+            locale: {
+              ...initialLocaleValue.currentState.locale,
+              lang: locale || enEsLocale,
+            },
+          },
+        },
+      },
+    });
+  };
+
+  it('should render header initial date text in English', () => {
+    renderHeader({});
+    const date = new Date(currentYear, currentMonth, currentDay);
+    const dayOfWeek = getDayOfWeek(enEsLocale, date);
+    const monthName = getMonthName(enEsLocale, date, IntlDateTimeFormatShort);
+    const plannerDateLabel = `${monthName} ${currentDay}, ${dayOfWeek}`;
+    const plannerDateLabelElement = screen.getByText(plannerDateLabel);
+    expect(plannerDateLabelElement).toBeInTheDocument();
+    expect(screen.getByText('Dec 1, Monday')).toBeInTheDocument();
+  });
+});
