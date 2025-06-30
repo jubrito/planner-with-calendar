f336c4c7b6b3f351651f417c41951996bf035eef
jubrito <jujubrito@outlook.com>
2025-06-29 23:36:46 -0300

    create EventCreationModalProps component

---
+++ src/features/Planner/components/EventModalsContainer/EventCreationModal/EventCreationModal.tsx
@@ -0,0 +1,31 @@
+import { memo, RefObject, useState } from 'react';
+import { Modal } from '../../../../../components/Modal/Modal';
+
+type EventCreationModalProps = {
+  closeModal: () => void;
+  createEventModalRef: RefObject<HTMLDivElement | null>;
+};
+
+export const EventCreationModal = memo(
+  ({ closeModal, createEventModalRef }: EventCreationModalProps) => {
+    const [title, setTitle] = useState('');
+
+    return (
+      <Modal
+        title={
+          <>
+            <input
+              placeholder="Add title"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+            />
+          </>
+        }
+        content={<p>content</p>}
+        style={{ top: 200 }} // TODO removes
+        closeModal={{ handleClose: closeModal }}
+        ref={createEventModalRef}
+      />
+    );
+  },
+);
