6ae00fdf0eb39ceddafebf76ca459523967b0d17
jubrito <jujubrito@outlook.com>
2025-07-13 22:37:18 -0300

    create useFocusManager hook for focus trapping

---
+++ src/hooks/useFocusManager.ts
@@ -0,0 +1,42 @@
+import { useRef, useCallback } from 'react';
+
+export function useFocusManager<T extends HTMLElement>(
+  activeElement: Element | null,
+) {
+  const lastActiveElement = useRef<HTMLElement>(null);
+  const elementRef = useRef<T>(null);
+
+  const focusFirstElement = useCallback(() => {
+    console.log('elementRef.current', elementRef.current);
+    if (elementRef.current == null) return;
+
+    const focusableElements = elementRef.current.querySelectorAll(
+      'button, [href], input, select, textarea',
+    );
+
+    console.log('focusableElements', focusableElements);
+    if (focusableElements.length > 0) {
+      const [firstElement] = focusableElements;
+      if (firstElement instanceof HTMLElement) {
+        firstElement.focus();
+      }
+    }
+  }, []);
+
+  const setupFocusTrap = useCallback(() => {
+    // Store the element that had focus before element opened to focus it back when it closes
+    lastActiveElement.current = activeElement as HTMLElement;
+    focusFirstElement();
+  }, [activeElement, focusFirstElement]);
+
+  const returnFocusToInitialElement = () => {
+    lastActiveElement.current?.focus();
+    lastActiveElement.current = null;
+  };
+
+  return {
+    elementRef,
+    setupFocusTrap,
+    returnFocusToInitialElement,
+  };
+}
