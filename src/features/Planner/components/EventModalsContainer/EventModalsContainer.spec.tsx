7214bcf473b3cef21779baeb46e6971cbee2e9b8
jubrito <jujubrito@outlook.com>
2025-07-13 23:44:00 -0300

    only check if event details modal is being rendered

--- src/features/Planner/components/EventModalsContainer/EventModalsContainer.spec.tsx
+++ src/features/Planner/components/EventModalsContainer/EventModalsContainer.spec.tsx
@@ -6,46 +6,27 @@ import { initialValue } from '../../../../redux/slices/eventSlice';
 import { getDateISOString } from '../../../../utils/calendar/utils';
 
 describe('EventModalsContainer', () => {
-  const refMock = { current: null };
   const eventTitle = 'eventTitle';
-  it('should not render modal if selected event is not defined', () => {
-    renderWithProviders(
-      <EventDetailsModal
-        editModal={jest.fn()}
-        closeModal={jest.fn()}
-        viewEventModalRef={refMock}
-      />,
-    );
-    const event = screen.queryByText(eventTitle);
-    expect(event).not.toBeInTheDocument();
-  });
-  it('should render modal if selected event is defined', () => {
-    renderWithProviders(
-      <EventDetailsModal
-        editModal={jest.fn()}
-        closeModal={jest.fn()}
-        viewEventModalRef={refMock}
-      />,
-      {
-        preloadedState: {
-          eventSlice: {
-            ...initialValue,
-            currentState: {
-              ...initialValue.currentState,
-              eventOnViewMode: {
-                event: {
-                  endDate: getDateISOString(new Date()),
-                  startDate: getDateISOString(new Date()),
-                  id: 'id',
-                  title: eventTitle,
-                },
-                top: 0,
+  it('should render event details modal', () => {
+    renderWithProviders(<EventDetailsModal />, {
+      preloadedState: {
+        eventSlice: {
+          ...initialValue,
+          currentState: {
+            ...initialValue.currentState,
+            eventOnViewMode: {
+              event: {
+                endDate: getDateISOString(new Date()),
+                startDate: getDateISOString(new Date()),
+                id: 'id',
+                title: eventTitle,
               },
+              top: 0,
             },
           },
         },
       },
-    );
+    });
     const event = screen.getByText(eventTitle);
     expect(event).toBeInTheDocument();
   });
