2bcecc69561d01361d34a9994b41de9fa84ca67c
jubrito <jujubrito@outlook.com>
2025-07-13 23:28:57 -0300

    test: should call function to return focus to initial element on closing modal

--- src/components/Modal/Modal.spec.tsx
+++ src/components/Modal/Modal.spec.tsx
@@ -1,8 +1,9 @@
 import { renderWithProviders } from '../../utils/tests/renderWithProviders';
 import { Modal } from './Modal';
-import { screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
+import * as useFocusManagerModule from '../../hooks/useFocusManager';
 
 describe('Modal', () => {
   const eventStyle = {};
@@ -16,109 +17,158 @@ describe('Modal', () => {
   const editLabel = 'Edit';
   const deleteLabel = 'Delete';
   let rerenderModal: (ui: React.ReactNode) => void;
+  const returnFocusMock = jest.fn();
 
-  beforeEach(() => {
-    const { rerender } = renderWithProviders(
-      <Modal
-        dialogAccessibleName={dialogAccessibleName}
-        style={eventStyle}
-        closeModal={{
-          closeLabel,
-          handleClose: closeModalMock,
-        }}
-        editModal={{
-          editLabel,
-          handleEdit: editModalMock,
-        }}
-        deleteModal={{
-          deleteLabel,
-          handleDelete: deleteModalMock,
-        }}
-        isOpen={true}
-      >
-        <>
-          <p>{eventTitle}</p>
-          <p>{content}</p>
-        </>
-      </Modal>,
-    );
-    rerenderModal = rerender;
-  });
-  it('should render modal title', () => {
-    expect(screen.getByText(eventTitle)).toBeInTheDocument();
-  });
-  it('should render modal content', () => {
-    expect(screen.getByText(content)).toBeInTheDocument();
-  });
-  it('should render actions labels as aria labels', () => {
-    expect(screen.getByLabelText(closeLabel)).toBeInTheDocument();
-    expect(screen.getByLabelText(editLabel)).toBeInTheDocument();
-    expect(screen.getByLabelText(deleteLabel)).toBeInTheDocument();
-  });
-  it('should render default aria-label if no label is provided', () => {
-    rerenderModal(
-      <Modal
-        style={{}}
-        dialogAccessibleName={dialogAccessibleName}
-        closeModal={{
-          handleClose: closeModalMock,
-        }}
-        editModal={{
-          handleEdit: editModalMock,
-        }}
-        deleteModal={{
-          handleDelete: deleteModalMock,
-        }}
-        isOpen={true}
-      >
-        {content}
-      </Modal>,
-    );
-    const closeDefaultLabel = 'Click to close modal';
-    const editDefaultLabel = 'Click to edit';
-    const deleteDefaultLabel = 'Click to delete';
+  describe('When modal is displayed', () => {
+    beforeEach(() => {
+      const { rerender } = renderWithProviders(
+        <Modal
+          dialogAccessibleName={dialogAccessibleName}
+          style={eventStyle}
+          closeModal={{
+            closeLabel,
+            handleClose: closeModalMock,
+          }}
+          editModal={{
+            editLabel,
+            handleEdit: editModalMock,
+          }}
+          deleteModal={{
+            deleteLabel,
+            handleDelete: deleteModalMock,
+          }}
+          isOpen={true}
+        >
+          <>
+            <p>{eventTitle}</p>
+            <p>{content}</p>
+          </>
+        </Modal>,
+      );
+      rerenderModal = rerender;
+    });
+    it('should render modal title', () => {
+      expect(screen.getByText(eventTitle)).toBeInTheDocument();
+    });
+    it('should render modal content', () => {
+      expect(screen.getByText(content)).toBeInTheDocument();
+    });
+    it('should render actions labels as aria labels', () => {
+      expect(screen.getByLabelText(closeLabel)).toBeInTheDocument();
+      expect(screen.getByLabelText(editLabel)).toBeInTheDocument();
+      expect(screen.getByLabelText(deleteLabel)).toBeInTheDocument();
+    });
+    it('should render default aria-label if no label is provided', () => {
+      rerenderModal(
+        <Modal
+          style={{}}
+          dialogAccessibleName={dialogAccessibleName}
+          closeModal={{
+            handleClose: closeModalMock,
+          }}
+          editModal={{
+            handleEdit: editModalMock,
+          }}
+          deleteModal={{
+            handleDelete: deleteModalMock,
+          }}
+          isOpen={true}
+        >
+          {content}
+        </Modal>,
+      );
+      const closeDefaultLabel = 'Click to close modal';
+      const editDefaultLabel = 'Click to edit';
+      const deleteDefaultLabel = 'Click to delete';
 
-    expect(screen.getByLabelText(closeDefaultLabel)).toBeInTheDocument();
-    expect(screen.getByLabelText(editDefaultLabel)).toBeInTheDocument();
-    expect(screen.getByLabelText(deleteDefaultLabel)).toBeInTheDocument();
+      expect(screen.getByLabelText(closeDefaultLabel)).toBeInTheDocument();
+      expect(screen.getByLabelText(editDefaultLabel)).toBeInTheDocument();
+      expect(screen.getByLabelText(deleteDefaultLabel)).toBeInTheDocument();
+    });
+    it('should render actions as buttons', () => {
+      const closeButton = screen.getByLabelText(closeLabel);
+      const editButton = screen.getByLabelText(editLabel);
+      const deleteButton = screen.getByLabelText(deleteLabel);
+      expect(closeButton).toHaveRole('button');
+      expect(editButton).toHaveRole('button');
+      expect(deleteButton).toHaveRole('button');
+    });
+    it('should focus modal first focusable element when component is displayed', () => {
+      const focusableElements = document.querySelectorAll(
+        'button, [href], input, select, textarea',
+      );
+      const [firstFocusableElement] = focusableElements;
+      expect(firstFocusableElement).toHaveFocus();
+    });
+    it('should set modal aria modal attribute to true', () => {
+      const modal = screen.getByRole('dialog');
+      expect(modal).toHaveAttribute('aria-modal', 'true');
+    });
+    it('should render modal with accessible name', () => {
+      const modal = screen.getByLabelText(dialogAccessibleName);
+      expect(modal).toBeInTheDocument();
+    });
   });
-  it('should render actions as buttons', () => {
-    const closeButton = screen.getByLabelText(closeLabel);
-    const editButton = screen.getByLabelText(editLabel);
-    const deleteButton = screen.getByLabelText(deleteLabel);
-    expect(closeButton).toHaveRole('button');
-    expect(editButton).toHaveRole('button');
-    expect(deleteButton).toHaveRole('button');
-  });
-  it('should call actions functions when clicking on buttons', async () => {
-    const closeButton = screen.getByLabelText(closeLabel);
-    const editButton = screen.getByLabelText(editLabel);
-    const deleteButton = screen.getByLabelText(deleteLabel);
+  describe('When running actions', () => {
+    beforeEach(() => {
+      jest
+        .spyOn(useFocusManagerModule, 'useFocusManager')
+        .mockImplementation(() => ({
+          elementRef: { current: null },
+          setupFocusTrap: jest.fn(),
+          returnFocusToInitialElement: returnFocusMock,
+        }));
+      renderWithProviders(
+        <Modal
+          dialogAccessibleName={dialogAccessibleName}
+          style={eventStyle}
+          closeModal={{
+            closeLabel,
+            handleClose: closeModalMock,
+          }}
+          editModal={{
+            editLabel,
+            handleEdit: editModalMock,
+          }}
+          deleteModal={{
+            deleteLabel,
+            handleDelete: deleteModalMock,
+          }}
+          isOpen={true}
+        >
+          <>
+            <p>{eventTitle}</p>
+            <p>{content}</p>
+          </>
+        </Modal>,
+      );
+    });
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
 
-    screen.debug();
+    it('should call actions functions when clicking on buttons', async () => {
+      const closeButton = screen.getByLabelText(closeLabel);
+      const editButton = screen.getByLabelText(editLabel);
+      const deleteButton = screen.getByLabelText(deleteLabel);
 
-    await userEvent.click(editButton);
-    await userEvent.click(deleteButton);
-    await userEvent.click(closeButton);
+      await userEvent.click(editButton);
+      await userEvent.click(deleteButton);
+      await userEvent.click(closeButton);
 
-    expect(closeModalMock).toHaveBeenCalledTimes(1);
-    expect(editModalMock).toHaveBeenCalledTimes(1);
-    expect(deleteModalMock).toHaveBeenCalledTimes(1);
+      expect(closeModalMock).toHaveBeenCalledTimes(1);
+      expect(editModalMock).toHaveBeenCalledTimes(1);
+      expect(deleteModalMock).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call function to return focus to initial element on closing modal', async () => {
+      const closeButton = screen.getByLabelText(closeLabel);
+
+      await act(async () => {
+        await userEvent.click(closeButton);
+      });
+
+      expect(returnFocusMock).toHaveBeenCalledTimes(1);
+    });
   });
-  it('should focus modal first focusable element when component is displayed', () => {
-    const focusableElements = document.querySelectorAll(
-      'button, [href], input, select, textarea',
-    );
-    const [firstFocusableElement] = focusableElements;
-    expect(firstFocusableElement).toHaveFocus();
-  });
-  it('should set modal aria modal attribute to true', () => {
-    const modal = screen.getByRole('dialog');
-    expect(modal).toHaveAttribute('aria-modal', 'true');
-  });
-  it('should render modal with accessible name', () => {
-    const modal = screen.getByLabelText(dialogAccessibleName);
-    expect(modal).toBeInTheDocument();
-  });
-  it.todo('should return focus to initial element on closing modal');
 });
