5c0a52beb6baca8731c4c67f51935bba0b577fef
jubrito <jujubrito@outlook.com>
2025-07-16 23:56:55 -0300

    handle errors when updating fields

--- src/hooks/useManageEventUpdates.tsx
+++ src/hooks/useManageEventUpdates.tsx
@@ -1,11 +1,13 @@
-import { useCallback, useState } from 'react';
-import { EventOnUpdateView } from '../types/event';
+import { useCallback, useEffect, useState } from 'react';
+import { EventOnUpdate } from '../types/event';
+import { validateEventOnUpdate } from '../utils/events/validation';
 
-export const useManageEventUpdates = (initialEvent?: EventOnUpdateView) => {
+export const useManageEventUpdates = (initialEvent?: EventOnUpdate) => {
   const now = new Date();
   const [isDirty, setIsDirty] = useState(false);
+  const [errors, setErrors] = useState({});
 
-  const [eventFields, setEventFields] = useState<EventOnUpdateView>({
+  const [eventFields, setEventFields] = useState<EventOnUpdate>({
     id: '',
     title: '',
     startDate: now,
@@ -16,20 +18,22 @@ export const useManageEventUpdates = (initialEvent?: EventOnUpdateView) => {
   });
 
   const updateEventField = useCallback(
-    <K extends keyof EventOnUpdateView>(
-      field: K,
-      newValue: EventOnUpdateView[K],
-    ) => {
+    <K extends keyof EventOnUpdate>(field: K, newValue: EventOnUpdate[K]) => {
       setEventFields((prevEventFields) => ({
         ...prevEventFields,
         [field]: newValue,
       }));
       setIsDirty(true);
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [field]: undefined,
+      }));
     },
     [],
   );
 
   return {
+    errors,
     isDirty,
     eventFields,
     updateEventField,
