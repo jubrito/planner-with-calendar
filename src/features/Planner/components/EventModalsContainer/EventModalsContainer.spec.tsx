a3fb801a8abbfdfc00ecdee3c48a39668f9ca80b
jubrito <jujubrito@outlook.com>
2025-07-17 00:27:22 -0300

    test: should render Event Update modal

--- src/features/Planner/components/EventModalsContainer/EventModalsContainer.spec.tsx
+++ src/features/Planner/components/EventModalsContainer/EventModalsContainer.spec.tsx
@@ -7,7 +7,8 @@ import { EventModalsContainer } from './EventModalsContainer';
 
 describe('EventModalsContainer', () => {
   const eventTitle = 'event title';
-  it('should render event details modal', () => {
+
+  it('should render Event Details modal', () => {
     renderWithProviders(<EventModalsContainer />, {
       preloadedState: {
         eventSlice: {
@@ -29,5 +30,29 @@ describe('EventModalsContainer', () => {
     });
     const event = screen.getByText(eventTitle);
     expect(event).toBeInTheDocument();
+  });
+
+  it('should render Event Update modal', () => {
+    renderWithProviders(<EventModalsContainer />, {
+      preloadedState: {
+        eventSlice: {
+          ...initialValue,
+          currentState: {
+            ...initialValue.currentState,
+            eventOnUpdate: {
+              event: {
+                endDate: getDateISOString(new Date()),
+                startDate: getDateISOString(new Date()),
+                id: 'id',
+                title: eventTitle,
+              },
+              top: 0,
+            },
+          },
+        },
+      },
+    });
+    const eventTitleInput = screen.getByPlaceholderText('Add title');
+    expect(eventTitleInput).toBeInTheDocument();
   });
 });
