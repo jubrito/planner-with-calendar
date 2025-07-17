621bf548448c1accba5a410855e51d58cb9801ea
jubrito <jujubrito@outlook.com>
2025-07-17 00:32:37 -0300

    create event creation modal

--- src/features/Planner/components/EventModalsContainer/EventCreationModal/EventCreationModal.tsx
+++ src/features/Planner/components/EventModalsContainer/EventCreationModal/EventCreationModal.tsx
@@ -1,31 +1,62 @@
-import { memo, RefObject, useState } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { memo, useCallback } from 'react';
 import { Modal } from '../../../../../components/Modal/Modal';
+import { clearEventOnUpdate } from '../../../../../redux/slices/eventSlice';
+import { useDispatch } from 'react-redux';
+import { useManageEventUpdates } from '../../../../../hooks/useManageEventUpdates';
+import { useSelector } from 'react-redux';
+import { getCurrentEventOnUpdate } from '../../../../../redux/slices/eventSlice/selectors';
+import { getLocaleLanguage } from '../../../../../redux/slices/localeSlice/selectors';
 
-type EventCreationModalProps = {
-  closeModal: () => void;
-  createEventModalRef: RefObject<HTMLDivElement | null>;
-};
+export const EventCreationModal = memo(() => {
+  const dispatch = useDispatch();
+  const eventOnUpdate = useSelector(getCurrentEventOnUpdate());
+  const locale = useSelector(getLocaleLanguage());
+  const isOpen = eventOnUpdate != null && eventOnUpdate.event != null;
 
-export const EventCreationModal = memo(
-  ({ closeModal, createEventModalRef }: EventCreationModalProps) => {
-    const [title, setTitle] = useState('');
+  const initialStartDate = eventOnUpdate?.event?.startDate
+    ? new Date(eventOnUpdate?.event?.startDate)
+    : undefined;
+  const initialEndDate = eventOnUpdate?.event?.endDate
+    ? new Date(eventOnUpdate?.event?.endDate)
+    : undefined;
 
-    return (
-      <Modal
-        title={
-          <>
-            <input
-              placeholder="Add title"
-              value={title}
-              onChange={(event) => setTitle(event.target.value)}
-            />
-          </>
-        }
-        content={<p>content</p>}
-        style={{ top: 200 }}
-        closeModal={{ handleClose: closeModal }}
-        ref={createEventModalRef}
-      />
-    );
-  },
-);
+  const {
+    eventFields,
+    isDirty,
+    errors,
+    updateEventField,
+    validateEventFields,
+  } = useManageEventUpdates({
+    ...eventOnUpdate?.event,
+    startDate: initialStartDate,
+    endDate: initialEndDate,
+  });
+  const { id, title, description, location, startDate, endDate } = eventFields;
+
+  const closeModal = useCallback(() => {
+    dispatch(clearEventOnUpdate());
+  }, [dispatch]);
+
+  const editModal = useCallback(() => {
+    alert('edit');
+  }, []);
+
+  return (
+    <Modal
+      style={{ top: 200 }}
+      closeModal={{ handleClose: closeModal }}
+      editModal={{ handleEdit: editModal }}
+      dialogAccessibleName={title}
+      isOpen={isOpen}
+    >
+      <>
+        <input
+          placeholder="Add title"
+          value={title}
+          onChange={(event) => updateEventField('title', event.target.value)}
+        />
+      </>
+    </Modal>
+  );
+});
