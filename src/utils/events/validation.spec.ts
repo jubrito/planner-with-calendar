e62485e8f9b8a7ac528dc80e1f638d8a57f7c8c4
jubrito <jujubrito@outlook.com>
2025-07-16 23:40:28 -0300

    rename const to invalidEventOnUpdate

--- src/utils/events/validation.spec.ts
+++ src/utils/events/validation.spec.ts
@@ -5,7 +5,7 @@ describe('validations', () => {
   describe('validateEventOnUpdate', () => {
     describe('Start date errors', () => {
       it('should return startDate error message if start date is not provided', () => {
-        const eventOnUpdate: Partial<EventOnUpdate> = {
+        const invalidEventOnUpdate: Partial<EventOnUpdate> = {
           id: '',
           title: '',
           startDate: undefined,
@@ -13,11 +13,11 @@ describe('validations', () => {
           location: '',
           description: '',
         };
-        const errors = validateEventOnUpdate(eventOnUpdate);
+        const errors = validateEventOnUpdate(invalidEventOnUpdate);
         expect(errors).toStrictEqual({ startDate: 'Start date is required' });
       });
       it('should return startDate error message if start date is invalid', () => {
-        const eventOnUpdate: Partial<EventOnUpdate> = {
+        const invalidEventOnUpdate: Partial<EventOnUpdate> = {
           id: '',
           title: '',
           startDate: new Date(0 / 0),
@@ -25,7 +25,7 @@ describe('validations', () => {
           location: '',
           description: '',
         };
-        const errors = validateEventOnUpdate(eventOnUpdate);
+        const errors = validateEventOnUpdate(invalidEventOnUpdate);
         expect(errors).toStrictEqual({
           startDate: 'Start date must be a valid date',
         });
@@ -33,7 +33,7 @@ describe('validations', () => {
     });
     describe('End date errors', () => {
       it('should return endDate error message if end date is not provided', () => {
-        const eventOnUpdate: Partial<EventOnUpdate> = {
+        const invalidEventOnUpdate: Partial<EventOnUpdate> = {
           id: '',
           title: '',
           startDate: new Date(),
@@ -41,11 +41,11 @@ describe('validations', () => {
           location: '',
           description: '',
         };
-        const errors = validateEventOnUpdate(eventOnUpdate);
+        const errors = validateEventOnUpdate(invalidEventOnUpdate);
         expect(errors).toStrictEqual({ endDate: 'End date is required' });
       });
       it('should return endDate error message if end date is invalid', () => {
-        const eventOnUpdate: Partial<EventOnUpdate> = {
+        const invalidEventOnUpdate: Partial<EventOnUpdate> = {
           id: '',
           title: '',
           startDate: new Date(),
@@ -53,13 +53,13 @@ describe('validations', () => {
           location: '',
           description: '',
         };
-        const errors = validateEventOnUpdate(eventOnUpdate);
+        const errors = validateEventOnUpdate(invalidEventOnUpdate);
         expect(errors).toStrictEqual({
           endDate: 'End date must be a valid date',
         });
       });
       it('should return endDate error message if end date is before start date', () => {
-        const eventOnUpdate: Partial<EventOnUpdate> = {
+        const invalidEventOnUpdate: Partial<EventOnUpdate> = {
           id: '',
           title: '',
           startDate: new Date(),
@@ -67,7 +67,7 @@ describe('validations', () => {
           location: '',
           description: '',
         };
-        const errors = validateEventOnUpdate(eventOnUpdate);
+        const errors = validateEventOnUpdate(invalidEventOnUpdate);
         expect(errors).toStrictEqual({
           endDate: 'End date must be after start date',
         });
