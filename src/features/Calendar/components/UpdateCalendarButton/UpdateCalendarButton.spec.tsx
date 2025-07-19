29e354119196957a8fd6c4591fb3553b06d91926
jubrito <jujubrito@outlook.com>
2025-07-19 03:29:04 -0300

    test: should display icon wrapped on button with label provided if icon exists

--- src/features/Calendar/components/UpdateCalendarButton/UpdateCalendarButton.spec.tsx
+++ src/features/Calendar/components/UpdateCalendarButton/UpdateCalendarButton.spec.tsx
@@ -8,7 +8,7 @@ describe('UpdateCalendarButton', () => {
   const labelMock = 'label-mock';
   const symbolMock = 'symbol-mock';
 
-  beforeEach(() => {
+  it('should display symbol wrapped on button with label provided if symbol exists', () => {
     render(
       <UpdateCalendarButton
         label={labelMock}
@@ -16,16 +16,31 @@ describe('UpdateCalendarButton', () => {
         updateDate={updateDateMock}
       />,
     );
-  });
-
-  it('should display symbol wrapped on button with label provided', () => {
     const buttonWithLabel = screen.getByRole('button', { name: labelMock });
     const symbol = within(buttonWithLabel).getByText(symbolMock);
     expect(buttonWithLabel).toBeInTheDocument();
     expect(symbol).toBeInTheDocument();
   });
 
+  it('should display icon wrapped on button with label provided if icon exists', () => {
+    const iconMock = 'Icon';
+    render(
+      <UpdateCalendarButton
+        label={labelMock}
+        icon={<p>{iconMock}</p>}
+        updateDate={updateDateMock}
+      />,
+    );
+    const buttonWithLabel = screen.getByRole('button', { name: labelMock });
+    const icon = within(buttonWithLabel).getByText(iconMock);
+    expect(buttonWithLabel).toBeInTheDocument();
+    expect(icon).toBeInTheDocument();
+  });
+
   it('should call updateDate function when clicking on button', async () => {
+    render(
+      <UpdateCalendarButton label={labelMock} updateDate={updateDateMock} />,
+    );
     const buttonWithLabel = screen.getByRole('button', { name: labelMock });
     await userEvent.click(buttonWithLabel);
     expect(updateDateMock).toHaveBeenCalledTimes(1);
