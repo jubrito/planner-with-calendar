0db2523d1f2f89762060c9cdfb665bc209c38ba6
jubrito <jujubrito@outlook.com>
2025-07-21 23:45:13 -0300

    test: should hide modal with event details when selecting a different date by clicking on a cell

--- src/features/Calendar/Calendar.spec.tsx
+++ src/features/Calendar/Calendar.spec.tsx
@@ -1,13 +1,18 @@
 import {
+  getDateISOString,
   getFormattedDateString,
   getMonthName,
 } from '../../utils/calendar/utils';
+import { initialValue as initialEventValue } from '../../redux/slices/eventSlice';
 import { getWeekDaysNames } from '../../utils/calendar/weeks';
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/dom';
 import { renderWithProviders } from '../../utils/tests/renderWithProviders';
 import Calendar from './Calendar';
 import { initialValue } from '../../redux/slices/dateSlice';
+import { Months } from '../../types/calendar/enums';
+import { EventStored } from '../../types/event';
+import userEvent from '@testing-library/user-event';
 
 describe('Calendar', () => {
   const englishLocale = 'en-us';
@@ -15,45 +20,101 @@ describe('Calendar', () => {
   const year = date.getFullYear();
   const ISODate = getFormattedDateString(englishLocale, date);
 
-  beforeEach(() => {
-    renderWithProviders(<Calendar />, {
-      preloadedState: {
-        dateSlice: {
-          ...initialValue,
-          currentState: {
-            ...initialValue.currentState,
-            globalISODate: ISODate,
-            dayViewISODate: ISODate,
+  describe('Rendering', () => {
+    beforeEach(() => {
+      renderWithProviders(<Calendar />, {
+        preloadedState: {
+          dateSlice: {
+            ...initialValue,
+            currentState: {
+              ...initialValue.currentState,
+              globalISODate: ISODate,
+              dayViewISODate: ISODate,
+            },
           },
         },
-      },
+      });
+    });
+
+    it('should render calendar header', () => {
+      const currentMonthName = getMonthName(englishLocale, date);
+      const weekDays = getWeekDaysNames(englishLocale);
+
+      expect(
+        screen.getByText(`${currentMonthName}, ${year}`),
+      ).toBeInTheDocument();
+      weekDays.forEach((weekDay) => {
+        expect(screen.getByText(weekDay.short)).toBeInTheDocument();
+      });
+    });
+
+    it('should render weeks', () => {
+      const weekDays = getWeekDaysNames(englishLocale);
+      weekDays.forEach((weekDay) => {
+        expect(screen.getByText(weekDay.short)).toBeInTheDocument();
+      });
+    });
+
+    it('should render calendar actions', () => {
+      expect(screen.getByLabelText('Go to previous year')).toBeInTheDocument();
+      expect(screen.getByLabelText('Go to previous month')).toBeInTheDocument();
+      expect(screen.getByText('Today'));
+      expect(screen.getByLabelText('Go to next month')).toBeInTheDocument();
+      expect(screen.getByLabelText('Go to next year')).toBeInTheDocument();
     });
   });
 
-  it('should render calendar header', () => {
-    const currentMonthName = getMonthName(englishLocale, date);
-    const weekDays = getWeekDaysNames(englishLocale);
+  describe('Actions', () => {
+    const cellYear = 2025;
+    const cellDay = 1;
+    const currentMonth = Months.JANUARY;
 
-    expect(
-      screen.getByText(`${currentMonthName}, ${year}`),
-    ).toBeInTheDocument();
-    weekDays.forEach((weekDay) => {
-      expect(screen.getByText(weekDay.short)).toBeInTheDocument();
+    it('should hide modal with event details when selecting a different date by clicking on a cell', async () => {
+      const initialEvent: EventStored = {
+        id: '',
+        title: '',
+        endDate: '',
+        startDate: '',
+      };
+      const initialEventOnViewMode = {
+        event: initialEvent,
+        top: 101,
+      };
+      const { store } = renderWithProviders(<Calendar />, {
+        preloadedState: {
+          dateSlice: {
+            ...initialValue,
+            currentState: {
+              ...initialValue.currentState,
+              globalISODate: getDateISOString(
+                new Date(cellYear, currentMonth, cellDay),
+              ),
+            },
+          },
+          eventSlice: {
+            ...initialEventValue,
+            currentState: {
+              ...initialEventValue.currentState,
+              eventOnViewMode: initialEventOnViewMode,
+            },
+          },
+        },
+      });
+      const buttonElement = screen.getByLabelText(
+        `Open Feb ${cellDay} of ${cellYear} day view`,
+      );
+      let eventOnViewMode =
+        store.getState().eventSlice.currentState.eventOnViewMode;
+
+      expect(eventOnViewMode?.top).toBe(initialEventOnViewMode.top);
+      expect(eventOnViewMode?.event).toBe(initialEventOnViewMode.event);
+
+      await userEvent.click(buttonElement);
+
+      eventOnViewMode =
+        store.getState().eventSlice.currentState.eventOnViewMode;
+
+      expect(eventOnViewMode).toBeUndefined();
     });
-  });
-
-  it('should render weeks', () => {
-    const weekDays = getWeekDaysNames(englishLocale);
-    weekDays.forEach((weekDay) => {
-      expect(screen.getByText(weekDay.short)).toBeInTheDocument();
-    });
-  });
-
-  it('should render calendar actions', () => {
-    expect(screen.getByLabelText('Go to previous year')).toBeInTheDocument();
-    expect(screen.getByLabelText('Go to previous month')).toBeInTheDocument();
-    expect(screen.getByText('Today'));
-    expect(screen.getByLabelText('Go to next month')).toBeInTheDocument();
-    expect(screen.getByLabelText('Go to next year')).toBeInTheDocument();
   });
 });
