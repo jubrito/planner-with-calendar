0b3dc17ba70d21907de8de90d8faff2c906bb138
jubrito <jujubrito@outlook.com>
2025-07-21 23:43:58 -0300

    display week initials if it is compact mode

--- src/features/Calendar/components/CalendarWeeks/CalendarWeeks.tsx
+++ src/features/Calendar/components/CalendarWeeks/CalendarWeeks.tsx
@@ -3,7 +3,10 @@ import { getWeekDaysNames } from '../../../../utils/calendar/weeks';
 import styles from './_calendar-weeks.module.scss';
 import { getLocaleLanguage } from '../../../../redux/slices/localeSlice/selectors';
 
-const CalendarWeeks = () => {
+type CalendarWeeksProps = {
+  compactMode?: boolean;
+};
+const CalendarWeeks = ({ compactMode = false }: CalendarWeeksProps) => {
   const localeLang = useSelector(getLocaleLanguage());
   const weekDays = getWeekDaysNames(localeLang);
   return (
@@ -18,12 +21,19 @@ const CalendarWeeks = () => {
               key={weekDay.long}
             >
               <abbr key={weekDay.long} title={weekDay.long} role="columnheader">
-                <span aria-hidden="true" className={styles.shortWeekName}>
-                  {weekDay.short}
-                </span>
-                <span aria-hidden="true" className={styles.initialWeekName}>
-                  {weekDay.initial}
-                </span>
+                {compactMode && (
+                  <span aria-hidden="true">{weekDay.initial}</span>
+                )}
+                {!compactMode && (
+                  <>
+                    <span aria-hidden="true" className={styles.shortWeekName}>
+                      {weekDay.short}
+                    </span>
+                    <span aria-hidden="true" className={styles.initialWeekName}>
+                      {weekDay.initial}
+                    </span>
+                  </>
+                )}
               </abbr>
             </th>
           );
