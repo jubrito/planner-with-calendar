bd7b6dda34e2881d4369c0a96fe8f2e1cc396982
jubrito <jujubrito@outlook.com>
2025-07-22 00:45:17 -0300

    test: should render buttons to enable changing calendar dates

---
+++ src/components/Calendar/CalendarActions/CalendarActions.spec.tsx
@@ -0,0 +1,53 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { CalendarActions } from './CalendarActions';
+
+describe('CalendarActions', () => {
+  describe('Changing calendar dates', () => {
+    const year = 2025;
+    const goToPreviousYearLabel = 'Previous year';
+    const goToPreviousMonthLabel = 'Previous month';
+    const goToNextMonthLabel = 'Next month';
+    const goToNextYearLabel = 'Next year';
+    const goToTodayLabel = 'Go to today';
+    const updateMonth = {
+      previous: jest.fn(),
+      next: jest.fn(),
+    };
+    const updateYear = {
+      previous: jest.fn(),
+      next: jest.fn(),
+    };
+    const updateToday = jest.fn();
+
+    beforeEach(() => {
+      render(
+        <CalendarActions
+          updateMonth={updateMonth}
+          updateYear={updateYear}
+          updateToday={updateToday}
+        />,
+      );
+    });
+
+    it('should render buttons to enable changing calendar dates', () => {
+      const goToPreviousYearButton = screen.getByRole('button', {
+        name: goToPreviousYearLabel,
+      });
+      const goToPreviousMonthButton = screen.getByRole('button', {
+        name: goToPreviousMonthLabel,
+      });
+      const goToNextMonthButton = screen.getByRole('button', {
+        name: goToNextMonthLabel,
+      });
+      const goToNextYearButton = screen.getByRole('button', {
+        name: goToNextYearLabel,
+      });
+
+      expect(goToPreviousYearButton).toBeInTheDocument();
+      expect(goToPreviousMonthButton).toBeInTheDocument();
+      expect(goToNextMonthButton).toBeInTheDocument();
+      expect(goToNextYearButton).toBeInTheDocument();
+    });
+  });
+});
