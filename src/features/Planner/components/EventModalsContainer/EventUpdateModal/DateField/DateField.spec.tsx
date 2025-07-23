89d44d6e3c0b6a85e743805a6b7b807c1f7eba70
jubrito <jujubrito@outlook.com>
2025-07-22 23:35:35 -0300

    test: should render icon

---
+++ src/features/Planner/components/EventModalsContainer/EventUpdateModal/DateField/DateField.spec.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { DateField } from './DateField';
+
+describe('DateField', () => {
+  const label = {
+    dateField: 'startDateLabel',
+    hourField: 'startHourLabel',
+  };
+  const value = {
+    dateField: '0',
+    hourField: 1,
+  };
+  const errorMessage = 'Error message';
+  const onClick = {
+    dateField: jest.fn(),
+    hourField: jest.fn(),
+  };
+  const icon = 'icon';
+
+  beforeEach(() => {
+    render(
+      <DateField
+        label={label}
+        value={value}
+        errorMessage={errorMessage}
+        className={{ wrapper: '', field: '' }}
+        onClick={onClick}
+        icon={<>{icon}</>}
+        readonly
+      />,
+    );
+  });
+  it('should render icon', () => {
+    expect(screen.getByText(icon)).toBeInTheDocument();
+  });
+});
