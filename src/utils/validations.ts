bd3f64525ad51e63c53db198612fffca816f259e
jubrito <jujubrito@outlook.com>
2025-08-02 00:19:59 -0300

    remove unused function

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -2,17 +2,6 @@ import { DateConfig } from '../types/calendar/types';
 import { LocaleLanguage } from '../types/locale/types';
 import { isValidDate, isValidLocale } from './checkers';
 
-export const validateDateTimeFormatRequirements = (
-  date: DateConfig['date'],
-  locale: LocaleLanguage,
-  valueName: string,
-) => {
-  if (!isValidDate(date))
-    throw new Error(`Failed to ${valueName}, date is invalid`);
-  if (!isValidLocale(locale))
-    throw new Error(`Failed to ${valueName}, language is invalid`);
-};
-
 export const validateDate = (date: DateConfig['date'], valueName: string) => {
   if (!isValidDate(date))
     throw new Error(`Failed to ${valueName}, date is invalid`);
