0b191f955186aa4e37e7ae6e1516d2955405f715
jubrito <jujubrito@outlook.com>
2025-08-12 23:19:25 -0300

    test: should render calendar based on date field year and month

--- src/components/Fields/DateCalendarField/DateCalendarField.spec.tsx
+++ src/components/Fields/DateCalendarField/DateCalendarField.spec.tsx
@@ -19,11 +19,12 @@ describe('DateCalendarField', () => {
   const someDay = 27;
   const date = new Date(defaultYear, defaultMonth, someDay);
   const initialISODate = getDateISOString(date);
+  const someCellLabel = `Dec ${someDay} of ${defaultYear}`;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let container: HTMLElement;
 
-  beforeEach(() => {
-    const { container: HTMLContainer } = renderWithProviders(
+  it('should render calendar based on date field year and month', async () => {
+    renderWithProviders(
       <DateCalendarField
         dateLabel={dateLabel}
         className={className}
@@ -34,67 +35,89 @@ describe('DateCalendarField', () => {
         initialISODate={initialISODate}
       />,
     );
-    container = HTMLContainer;
-  });
-  it('should render start date field label with id', () => {
-    const dateField = screen.getByLabelText(dateLabel);
-    expect(dateField).toBeInTheDocument();
-    expect(dateField.id).toBe(dateLabel);
-  });
-
-  it('should render date field with read only properties as true', () => {
-    const dateField = screen.getByLabelText(dateLabel);
-    expect(dateField).toHaveAttribute('readonly');
-    expect(dateField).toHaveAttribute('aria-readonly', 'true');
-  });
-
-  it('should render end date field with aria error message', () => {
-    const dateField = screen.getByLabelText(dateLabel);
-    expect(dateField).toHaveAttribute('aria-errormessage', errorMessage);
-  });
-
-  it('should render date field as input', () => {
-    const dateField = screen.getByRole('textbox', {
-      name: dateLabel,
-    });
-    expect(dateField).toBeInTheDocument();
-  });
-
-  it('should display date calendar when clicking on date input (mouse)', async () => {
-    const dateField = screen.getByLabelText(dateLabel);
-    await userEvent.click(dateField);
-    const calendar = screen.getByRole('table');
-    expect(calendar).toBeInTheDocument();
-  });
-  it('should display date calendar when clicking on date input (key down)', async () => {
-    const dateField = screen.getByLabelText(dateLabel);
-    fireEvent.keyDown(dateField, { key: enterKey });
-    const calendar = screen.getByRole('table');
-    expect(calendar).toBeInTheDocument();
-  });
-
-  it('should call date on click function when clicking on calendar (mouse)', async () => {
     const dateField = screen.getByLabelText(dateLabel);
     await userEvent.click(dateField);
     const calendar = screen.getByRole('table');
     const cellButton = within(calendar).getByRole('button', {
-      name: `Dec ${someDay} of ${defaultYear}`,
+      name: someCellLabel,
     });
-    await userEvent.click(cellButton);
-    expect(onCellClick).toHaveBeenCalled();
+    expect(cellButton).toBeInTheDocument();
   });
 
-  it('should call date on click function when clicking on calendar (key down)', () => {
-    const dateField = screen.getByLabelText(dateLabel);
-    fireEvent.keyDown(dateField, { key: enterKey });
-    expect(onCellClick).toHaveBeenCalled();
-  });
+  describe('Default rendering', () => {
+    beforeEach(() => {
+      const { container: HTMLContainer } = renderWithProviders(
+        <DateCalendarField
+          dateLabel={dateLabel}
+          className={className}
+          value={value}
+          errorMessage={errorMessage}
+          isFieldReadOnly={isFieldReadOnly}
+          onCellClick={onCellClick}
+          initialISODate={initialISODate}
+        />,
+      );
+      container = HTMLContainer;
+    });
+    it('should render start date field label with id', () => {
+      const dateField = screen.getByLabelText(dateLabel);
+      expect(dateField).toBeInTheDocument();
+      expect(dateField.id).toBe(dateLabel);
+    });
 
-  it.skip('should hide date calendar when clicking on date input (mouse)', async () => {
-    const dateField = screen.getByLabelText(dateLabel);
-    await userEvent.click(dateField);
-    const calendar = screen.getByRole('table');
-    expect(calendar).toBeInTheDocument();
+    it('should render date field with read only properties as true', () => {
+      const dateField = screen.getByLabelText(dateLabel);
+      expect(dateField).toHaveAttribute('readonly');
+      expect(dateField).toHaveAttribute('aria-readonly', 'true');
+    });
+
+    it('should render end date field with aria error message', () => {
+      const dateField = screen.getByLabelText(dateLabel);
+      expect(dateField).toHaveAttribute('aria-errormessage', errorMessage);
+    });
+
+    it('should render date field as input', () => {
+      const dateField = screen.getByRole('textbox', {
+        name: dateLabel,
+      });
+      expect(dateField).toBeInTheDocument();
+    });
+
+    it('should display date calendar when clicking on date input (mouse)', async () => {
+      const dateField = screen.getByLabelText(dateLabel);
+      await userEvent.click(dateField);
+      const calendar = screen.getByRole('table');
+      expect(calendar).toBeInTheDocument();
+    });
+    it('should display date calendar when clicking on date input (key down)', async () => {
+      const dateField = screen.getByLabelText(dateLabel);
+      fireEvent.keyDown(dateField, { key: enterKey });
+      const calendar = screen.getByRole('table');
+      expect(calendar).toBeInTheDocument();
+    });
+
+    it('should call date on click function when clicking on calendar (mouse)', async () => {
+      const dateField = screen.getByLabelText(dateLabel);
+      await userEvent.click(dateField);
+      const calendar = screen.getByRole('table');
+      const cellButton = within(calendar).getByRole('button', {
+        name: someCellLabel,
+      });
+      await userEvent.click(cellButton);
+      expect(onCellClick).toHaveBeenCalled();
+    });
+
+    it('should call date on click function when clicking on calendar (key down)', () => {
+      const dateField = screen.getByLabelText(dateLabel);
+      fireEvent.keyDown(dateField, { key: enterKey });
+      expect(onCellClick).toHaveBeenCalled();
+    });
+
+    it.todo('should hide date calendar when clicking on date input (mouse)');
+    // const dateField = screen.getByLabelText(dateLabel);
+    // await userEvent.click(dateField);
+    // const calendar = screen.getByRole('table');
+    // expect(calendar).toBeInTheDocument();
     // const dayViewContainer = container.firstElementChild;
   });
 });
