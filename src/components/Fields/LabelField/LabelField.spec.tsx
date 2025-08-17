d562a772c428f3352490f256d28068e42e5c178c
jubrito <jujubrito@outlook.com>
2025-08-17 19:16:22 -0300

    test: should hide label visually but not from screen readers when srOnly (screen readers only) is true

---
+++ src/components/Fields/LabelField/LabelField.spec.tsx
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/dom';
+import { LabelField } from './LabelField';
+import { render } from '@testing-library/react';
+
+describe('LabelField', () => {
+  const label = 'label';
+  const htmlFor = 'htmlFor';
+  const className = 'className';
+
+  it('should hide label visually but not from screen readers when srOnly (screen readers only) is true', () => {
+    render(
+      <LabelField
+        label={label}
+        htmlFor={htmlFor}
+        className={className}
+        srOnly
+      />,
+    );
+    const labelField = screen.getByText(label);
+    expect(labelField).toBeInTheDocument();
+    expect(labelField).not.toBeVisible();
+    expect(labelField).toHaveAttribute('aria-hidden', 'true');
+  });
+});
