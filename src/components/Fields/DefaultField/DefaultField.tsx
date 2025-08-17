f8c319be528e242031142e954c8c1b580221618e
jubrito <jujubrito@outlook.com>
2025-08-17 17:35:31 -0300

    update value based on value received as prop

--- src/components/Fields/DefaultField/DefaultField.tsx
+++ src/components/Fields/DefaultField/DefaultField.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import sharedStyles from '../../../styles/shared.module.scss';
 
 type DefaultField = {
@@ -11,7 +11,7 @@ type DefaultField = {
   placeholder?: string;
   value: string;
   readOnly?: boolean;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onClick?: (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => void;
   onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
   errorMessage?: string;
@@ -33,6 +33,11 @@ export const DefaultField = ({
   const labelId = `${id}-label`;
   const [inputValue, setInputValue] = useState(value);
 
+  useEffect(() => {
+    if (value === inputValue) return;
+    setInputValue(value);
+  }, [value, inputValue]);
+
   return (
     <>
       <label
@@ -50,6 +55,7 @@ export const DefaultField = ({
         placeholder={placeholder}
         value={inputValue}
         onChange={(event) => {
+          if (!onChange || readOnly) return;
           onChange(event);
           setInputValue(event.target.value);
         }}
