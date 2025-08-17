411b07a9552fbf469c192b5ba01d007fdcaba8fd
jubrito <jujubrito@outlook.com>
2025-08-17 20:37:55 -0300

    combine classnames

--- src/components/Fields/DefaultField/DefaultField.tsx
+++ src/components/Fields/DefaultField/DefaultField.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import sharedStyles from '../../../styles/shared.module.scss';
 import { LabelField } from '../LabelField/LabelField';
+import styles from './_default-field.module.scss';
+import { ObjectType } from '../../../types/types';
 
 type DefaultField = {
   className?: string;
@@ -16,6 +18,7 @@ type DefaultField = {
   onClick?: (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => void;
   onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
   errorMessage?: string;
+  inputStyle?: ObjectType;
 };
 
 export const DefaultField = ({
@@ -28,11 +31,13 @@ export const DefaultField = ({
   id,
   label,
   placeholder,
+  inputStyle,
   readOnly = false,
   ...props
 }: DefaultField) => {
   const inputId = id.replace(' ', '-');
   const [inputValue, setInputValue] = useState(value);
+  const classNames = `${className || ''} ${styles.line} ${styles.box}`;
 
   useEffect(() => {
     const onlyUpdatesBasedOnProps = !readOnly || value === inputValue;
@@ -41,16 +46,16 @@ export const DefaultField = ({
   }, [readOnly, value, inputValue]);
 
   return (
-    <>
+    <div className={styles.defaultField}>
       <LabelField
         htmlFor={inputId}
         aria-hidden={label.srOnly}
         srOnly={label.srOnly}
-        className={label.srOnly ? sharedStyles.srOnly : className}
+        className={label.srOnly ? sharedStyles.srOnly : undefined}
         label={label.text}
       />
       <input
-        className={className}
+        className={classNames}
         id={inputId}
         placeholder={placeholder}
         value={inputValue}
@@ -64,8 +69,9 @@ export const DefaultField = ({
         aria-errormessage={errorMessage}
         aria-readonly={readOnly}
         readOnly={readOnly}
+        style={inputStyle}
         {...props}
       />
-    </>
+    </div>
   );
 };
