62059d76a72b3ef12e68ab5fc07772311d0a3b2d
jubrito <jujubrito@outlook.com>
2025-08-19 00:58:32 -0300

    test todo: should display end date error

--- src/components/Fields/DateCalendarField/DateCalendarField.spec.tsx
+++ src/components/Fields/DateCalendarField/DateCalendarField.spec.tsx
@@ -134,7 +134,7 @@ describe('DateCalendarField', () => {
     });
     it.todo('should display start date error');
     it.todo('should display start hour error');
-
+    it.todo('should display end date error');
     it.todo(
       'should update end date with start date to ensure end date is greater than start date',
     );
