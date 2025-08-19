7a513160b6d0c99284cbf33cdcd9829594c142bf
jubrito <jujubrito@outlook.com>
2025-08-18 22:22:35 -0300

    restore move changes

--- src/components/Fields/TimeField/TimeField.spec.tsx
+++ src/components/Fields/TimeField/TimeField.spec.tsx
@@ -4,12 +4,60 @@ import { TimeField } from './TimeField';
 import { renderWithProviders } from '../../../utils/tests/renderWithProviders';
 import userEvent from '@testing-library/user-event';
 import { initialValue } from '../../../redux/slices/localeSlice';
-import {
-  hoursIn12HoursFormat,
-  hoursIn24HoursFormat,
-} from '../../../utils/locale/constants';
 
 describe('TimeField', () => {
+  const hoursIn12HoursFormat = [
+    '12 am',
+    '01 am',
+    '02 am',
+    '03 am',
+    '04 am',
+    '05 am',
+    '06 am',
+    '07 am',
+    '08 am',
+    '09 am',
+    '10 am',
+    '11 am',
+    '12 pm',
+    '01 pm',
+    '02 pm',
+    '03 pm',
+    '04 pm',
+    '05 pm',
+    '06 pm',
+    '07 pm',
+    '08 pm',
+    '09 pm',
+    '10 pm',
+    '11 pm',
+  ];
+  const hoursIn24HoursFormat = [
+    '00:00',
+    '01:00',
+    '02:00',
+    '03:00',
+    '04:00',
+    '05:00',
+    '06:00',
+    '07:00',
+    '08:00',
+    '09:00',
+    '10:00',
+    '11:00',
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00',
+    '23:00',
+  ];
   const id = 'id';
   const label = 'label';
   const value = 'value';
